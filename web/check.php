17d65fa54707cbe142912d19ae46c6caf238bc73
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-09 20:15:27 +0200

    also change failmessage to json

--- web/check.php
+++ web/check.php
@@ -9,7 +9,8 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
-    exit('This script is only accessible from localhost.');
+    header('Content-Type: application/json');
+    exit('{"fail":"This script is only accessible from localhost."}');
 }
 require_once dirname(__FILE__).'/../app/SymfonyRequirements.php';
 
