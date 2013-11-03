efb90adfab1c7963f5aea75a0a2420dfd8675e77
Lucas Bickel <hairmare@purplehaze.ch>
2013-11-03 13:46:50 +0100

    remove logic from app kernel
    moves bundle loading into a new class and makes CoreBundle and BundleBundle use GravitonBundleInterface.

--- src/Graviton/CoreBundle/GravitonCoreBundle.php
+++ src/Graviton/CoreBundle/GravitonCoreBundle.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * Core Bundle contains core infrastructure.
+ * core infrastructure like logging and framework.
  */
 
 namespace Graviton\CoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Graviton\BundleBundle\GravitonBundleInterface;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
 
 /**
  * GravitonCoreBundle
@@ -16,7 +21,22 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     http://swisscom.com
  */
-class GravitonCoreBundle extends Bundle
+class GravitonCoreBundle extends Bundle implements GravitonBundleInterface
 {
-
+    /**
+     * {@inheritDoc}
+     *
+     * set up a bare bones symfony2 context
+     *
+     * @return Array
+     */
+    public function getBundles()
+    {
+        return array(
+            new FrameworkBundle(),
+            new SecurityBundle(),
+            new MonologBundle(),
+            new SensioFrameworkExtraBundle(),
+        );
+    }
 }
