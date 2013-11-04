be1d9d1ac13f9ff784a8180ccb44b1a6459b6f56
Lucas Bickel <hairmare@purplehaze.ch>
2013-11-04 20:36:41 +0100

    odd based App document
    This shall replace the Entity\App in CoreBundle but I'm not mucking with
    that until this is up and running.

---
+++ src/Graviton/CoreBundle/Document/App.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Document for representing Apps.
+ */
+
+namespace Graviton\CoreBundle\Document;
+
+/**
+ * App
+ *
+ * @category GravitonCoreBundle
+ * @package  Graviton
+ * @author   Lucas Bickel <lucas.bickel@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
+class App
+{
+    /**
+     * @var MongoId $id
+     */
+    protected $id;
+
+    /**
+     * @var string $name
+     */
+    protected $name;
+
+    /**
+     * @var string $title
+     */
+    protected $title;
+
+    /**
+     * @var boolean $showInMenu
+     */
+    protected $showInMenu;
+
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return self
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string $title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set showInMenu
+     *
+     * @param boolean $showInMenu
+     * @return self
+     */
+    public function setShowInMenu($showInMenu)
+    {
+        $this->showInMenu = $showInMenu;
+        return $this;
+    }
+
+    /**
+     * Get showInMenu
+     *
+     * @return boolean $showInMenu
+     */
+    public function getShowInMenu()
+    {
+        return $this->showInMenu;
+    }
+}
