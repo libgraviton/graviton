4747e6619e7e0fe9fe247f846391363e5ba2802d
Lucas Bickel <hairmare@purplehaze.ch>
2014-05-15 21:58:39 +0200

    phpunit for BundleBundle integration

---
+++ src/Graviton/RestBundle/Tests/GravitonRestBundleTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * test rest-bundle
+ */
+
+namespace Graviton\RestBundle\Tests;
+
+use Graviton\RestBundle\GravitonRestBundle;
+use JMS\SerializerBundle\JMSSerializerBundle;
+use FOS\RestBundle\FOSRestBundle;
+
+/**
+ * GravitonMessagingBundleTest
+ * 
+ * @category Tests
+ * @package  GravitonMessagingBundle
+ * @author   Lucas Bickel <lucas.bickel@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
+class GravitonBundleRestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * check for implemented interfaces
+     *
+     * @return void
+     */
+    public function testIsInterface()
+    {
+        $this->assertInstanceOf(
+            '\Graviton\BundleBundle\GravitonBundleInterface',
+            new GravitonRestBundle()
+        );
+    }
+    /**
+     * test getBundles method
+     *
+     * @return void
+     */
+    public function testGetBundles()
+    {
+        $sut = new GravitonRestBundle();
+        $expectation = array(
+            new JMSSerializerBundle(),
+            new FOSRestBundle(),
+        );
+
+        $result = $sut->getBundles();
+        $this->assertEquals($expectation, $result);
+    }
+}
