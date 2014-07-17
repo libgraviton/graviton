609303b36034abbc8ff029a15c9dce61b34dede3
Lucas Bickel <hairmare@purplehaze.ch>
2014-07-17 12:41:11 +0200

    add missing comment

--- src/Graviton/SchemaBundle/Model/SchemaModel.php
+++ src/Graviton/SchemaBundle/Model/SchemaModel.php
@@ -2,6 +2,15 @@
 
 namespace Graviton\SchemaBundle\Model;
 
+/**
+ * Model based on Graviton\RestBundle\Model\DocumentModel.
+ *
+ * @category SchemaBundle
+ * @package  Graviton
+ * @author   Lucas Bickel <lucas.bickel@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
 class SchemaModel
 {
     /**
