6089aff7d2e9198deda835e80a00ddfeec2ef0e7
Lucas Bickel <hairmare@purplehaze.ch>
2014-07-28 16:41:18 +0200

    bundle scaffolding

--- src/Graviton/CoreBundle/GravitonCoreBundle.php
+++ src/Graviton/CoreBundle/GravitonCoreBundle.php
@@ -18,6 +18,7 @@ use Graviton\RestBundle\GravitonRestBundle;
 use Graviton\TestBundle\GravitonTestBundle;
 use Graviton\TaxonomyBundle\GravitonTaxonomyBundle;
 use Graviton\I18nBundle\GravitonI18nBundle;
+use Graviton\PersonBundle\GravitonPersonBundle;
 
 /**
  * GravitonCoreBundle
@@ -51,6 +52,7 @@ class GravitonCoreBundle extends Bundle implements GravitonBundleInterface
             new GravitonTestBundle(),
             new GravitonTaxonomyBundle(),
             new GravitonI18nBundle(),
+            new GravitonPersonBundle(),
         );
     }
 }
---
+++ src/Graviton/PersonBundle/GravitonPersonBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Graviton\PersonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * GravitonPersonBundle
+ *
+ * @category PersonBundle
+ * @package  Graviton
+ * @author   Lucas Bickel <lucas.bickel@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
+class GravitonPersonBundle extends Bundle
+{
+}
