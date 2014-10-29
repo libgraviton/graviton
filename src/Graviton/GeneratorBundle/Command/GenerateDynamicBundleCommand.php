91b67929fd5a817141232f5ce64c73b456a900b0
narcoticfresh <wee@xbe.ch>
2014-10-29 11:40:18 +0100

    more config in generatebundle; streamlining in dynamicbundle..

--- src/Graviton/GeneratorBundle/Command/GenerateDynamicBundleCommand.php
+++ src/Graviton/GeneratorBundle/Command/GenerateDynamicBundleCommand.php
@@ -12,13 +12,26 @@ use Graviton\GeneratorBundle\Generator\BundleGenerator;
 use Graviton\GeneratorBundle\Definition\JsonDefinition;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\Output;
 
 /**
- * generator command
+ * Here, we generate all "dynamic" Graviton bundles.. The workflow is as
+ * follows: * Generate a BundleBundle, implementing the GravitonBundleInterface
+ * * Generate our Bundles per JSON file * Creating the necessary resources and
+ * files inside the newly created bundles. * All that in our own GravitonDyn
+ * namespace. Important: Why are we using shell_exec instead of just using the
+ * internal API? Well, the main problem is, that we want to add resources (like
+ * Documents) to our Bundles *directly* after generating them. Using the
+ * internal API, we cannot add resources there using our tools as those Bundles
+ * haven't been loaded yet through the AppKernel. Using shell_exec we can do
+ * that.. This shouldn't be a dealbreaker as this task is only used on
+ * deployment and/or development where a shell is accessible. It should be
+ * executed in the same context as the previous generator tools, and also those
+ * used the shell (backtick operator to get git name/email for example).
  *
  * @category GeneratorBundle
  * @package Graviton
- * @author Lucas Bickel <lucas.bickel@swisscom.com>
+ * @author Dario Nuevo <dario.nuevo@swisscom.com>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link http://swisscom.ch
  */
@@ -34,9 +47,19 @@ class GenerateDynamicBundleCommand extends ContainerAwareCommand
     {
         parent::configure();
         
-        $this->addOption('json', '', InputOption::VALUE_OPTIONAL, 'Path to the json definition.')
-            ->setName('graviton:generate:dynamicbundle')
-            ->setDescription('Generates a graviton dynamic bundle from a json definition');
+        $this->addOption('json', '', InputOption::VALUE_OPTIONAL, 
+            'Path to the json definition.')
+            ->addOption('bundleNameMask', '', InputOption::VALUE_OPTIONAL, 
+            'Name mask', 'GravitonDyn/%sBundle')
+            ->addOption('srcDir', '', InputOption::VALUE_OPTIONAL, 
+            'Src Dir', dirname(__FILE__) . '/../../../')
+            ->addOption('bundleFormat', '', InputOption::VALUE_OPTIONAL,
+                'Which format', 'xml')            
+            ->setName('graviton:generate:dynamicbundles')
+            
+            ->setDescription(
+            'Generates all dynamic bundles in the GravitonDyn namespace. Either give a path
+                    to a single JSON file or a directory path containing multipl files.');
     }
 
     /**
@@ -46,61 +69,92 @@ class GenerateDynamicBundleCommand extends ContainerAwareCommand
      *            input
      * @param OutputInterface $output
      *            output
-     *            
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $jsonDef = new JsonDefinition($input->getOption('json'));
+        $bundleNameMask = $input->getOption('bundleNameMask');
         
         /**
-         * ** GENERATE THE BUNDLE ***
+         * GENERATE THE BUNDLEBUNDLE
          */
+        $namespace = sprintf($bundleNameMask, 'Bundle');
+        $this->generateBundle($namespace, $input, $output);        
         
-        // compose names
+        /**
+         * GENERATE THE BUNDLE
+         */
+        $jsonDef = new JsonDefinition($input->getOption('json'));
+        
         $thisIdName = ucfirst(strtolower($jsonDef->getId()));
-        $namespace = 'GravitonDyn/' . $thisIdName . 'Bundle';
-        $bundleName = str_replace('/', '', $namespace);
-        $baseDir = dirname(__FILE__) . '/../../../';
-        
-        // first, create the bundle
-        $command = $this->getApplication()->find('graviton:generate:bundle');
-        $arguments = array(
-            'graviton:generate:bundle',
-            '--namespace' => $namespace,
-            '--bundle-name' => $bundleName,
-            '--dir' => $baseDir,
-            '--format' => 'xml',
-            '--structure' => 'true'
-        );
-        
-        $input = new ArrayInput($arguments);
-        $input->setInteractive(false);
-        $returnCode = $command->run($input, $output);
-        
+        $namespace = sprintf($bundleNameMask, $thisIdName);
+        $this->generateBundle($namespace, $input, $output);  
+                
         /**
          * ** GENERATE THE RESOURCE ***
          */
         
-        $command = $this->getApplication()->find('graviton:generate:resource');
+        // $command =
+        // $this->getApplication()->find('graviton:generate:resource');
         $arguments = array(
             'graviton:generate:resource',
             '--entity' => $bundleName . ':' . $thisIdName,
             '--format' => 'xml',
             '--fields' => $this->getFieldString($jsonDef),
-            '--with-repository' => 'true'
+            '--with-repository' => null
         );
-        $input = new ArrayInput($arguments);
-        $input->setInteractive(false);
-        $returnCode = $command->run($input, $output);
+        $this->executeCommand($arguments, $output);
         
-        // php app/console graviton:generate:resource --entity=GravitonFooBundle:Baz --format=xml \
-        // --fields="name:string isTrue:boolean consultant:Graviton\\PersonBundle\\Document\\Consultant valid:boolean contacts:Graviton\\PersonBundle\\Document\\PersonContact[] tags:array" \
+        // $input = new ArrayInput($arguments);
+        // $input->setInteractive(false);
+        // $returnCode = $command->run($input, $output);
+        
+        // php app/console graviton:generate:resource
+        // --entity=GravitonFooBundle:Baz --format=xml \
+        // --fields="name:string isTrue:boolean
+        // consultant:Graviton\\PersonBundle\\Document\\Consultant valid:boolean
+        // contacts:Graviton\\PersonBundle\\Document\\PersonContact[]
+        // tags:array" \
         // --with-repository --no-interaction
         
         $output->writeln('Generated the bundle and the resource.');
     }
+    
+    private function generateBundle($namespace, InputInterface $input, OutputInterface $output)
+    {
+        $bundleName = str_replace('/', '', $namespace);
+        
+        // first, create the bundle
+        $arguments = array(
+            'graviton:generate:bundle',
+            '--namespace' => $namespace,
+            '--bundle-name' => $bundleName,
+            '--dir' => $input->getOption('srcDir'),
+            '--format' => $input->getOption('bundleFormat'),
+            '--doUpdateKernel' => 'false',
+            '--structure' => null
+        );
+        
+        return $this->executeCommand($arguments, $output);
+    }
 
+    private function executeCommand(array $args, OutputInterface $output)
+    {
+        $cmd = 'php app/console -n ';
+        
+        foreach ($args as $key => $val) {
+            if (strlen($key) > 1)
+                $cmd .= ' ' . $key;
+            if (strlen($key) > 1 && ! is_null($val))
+                $cmd .= '=';
+            if (strlen($val) > 1)
+                $cmd .= escapeshellarg($val);
+        }
+        
+        $output->writeln(sprintf('Executing "%s"', $cmd));
+        return shell_exec($cmd);
+    }
+    
     /**
      * Returns the field string as described in the json file
      *
@@ -113,7 +167,7 @@ class GenerateDynamicBundleCommand extends ContainerAwareCommand
         
         foreach ($jsonDef->getFields() as $field) {
             // don't add 'id' field it seems..
-            if ($field->getName != 'id') {
+            if ($field->getName() != 'id') {
                 $ret[] = $field->getName() . ':' . $field->getTypeDoctrine();
             }
         }
