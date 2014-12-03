1310e7ed8d3d51dc68bfd5d7f269c5d3bc5b293a
Manuel Kipfer <manuel.kipfer@swisscom.com>
2014-12-03 11:09:59 +0100

    add patch action

---
+++ src/Graviton/RestBundle/Action/PatchAction.php
@@ -0,0 +1,17 @@
+<?php
+namespace Graviton\RestBundle\Action;
+
+/**
+ * Patch Action
+ *
+ * Nothing to do...
+ *
+ * @category RestBundle
+ * @package  Graviton
+ * @author   Manuel Kipfer <manuel.kipfer@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
+class PatchAction extends AbstractAction
+{
+}
