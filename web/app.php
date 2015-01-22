844a2011407fc2717d588a17d93062efecfe953c
Bastian Feder <bastian.feder@swisscom.com>
2015-01-22 14:25:39 +0100

    Enabled »prod« environment

--- web/app.php
+++ web/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Graviton\BundleBundle\GravitonBundleBundle;
+use Graviton\BundleBundle\Loader\BundleLoader;
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,11 +16,15 @@ $loader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new \Graviton\AppKernel('prod', false);
+//$kernel = new AppCache($kernel);
+
+// make sure graviton bundles are found and initialized
+$kernel->setBundleLoader(new BundleLoader(new GravitonBundleBundle()));
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
