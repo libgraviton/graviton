197c776457de6e435c7d00e522fd37e98d1179b3
narcoticfresh <wee@xbe.ch>
2015-02-05 11:23:45 +0100

    move swagger spec generation into own service (slimmer controller); finish up..

--- src/Graviton/RestBundle/Service/RestUtils.php
+++ src/Graviton/RestBundle/Service/RestUtils.php
@@ -2,10 +2,19 @@
 
 namespace Graviton\RestBundle\Service;
 
-
 use Graviton\RestBundle\Controller\RestController;
 use Symfony\Component\Routing\Route;
 
+/**
+ * A service (meaning symfony service) providing some convenience stuff when dealing with our RestController
+ * based services (meaning rest services).
+ *
+ * @category GravitonRestBundle
+ * @package  Graviton
+ * @author   Dario Nuevo <dario.nuevo@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
 class RestUtils
 {
 
@@ -79,18 +88,37 @@ class RestUtils
         return $ret;
     }
 
-    public function getControllerFromRoute(Route $route)
+    /**
+     * Based on $baseName, this function returns all routes that match this basename..
+     * So if you pass graviton.cont.action; it will return all route names that start with the same.
+     * In our routing naming schema, this means all the routes from the same controller.
+     *
+     * @param $baseName string basename
+     *
+     * @return array array with matching routes
+     */
+    public function getRoutesByBasename($baseName)
     {
-        $ret = false;
-        $actionParts = explode(':', $route->getDefault('_controller'));
-
-        if (count($actionParts) == 2) {
-            $ret = $this->container->get($actionParts[0]);
+        $ret = array();
+        foreach ($this->container->get('router')
+                                 ->getRouteCollection()
+                                 ->all() as $routeName => $route
+        ) {
+            if (preg_match('/^' . $baseName . '/', $routeName)) {
+                $ret[$routeName] = $route;
+            }
         }
-
         return $ret;
     }
 
+    /**
+     * Gets the Model assigned to the RestController
+     *
+     * @param Route $route Route
+     *
+     * @return bool|object The model or false
+     * @throws \Exception
+     */
     public function getModelFromRoute(Route $route)
     {
         $ret = false;
@@ -104,22 +132,21 @@ class RestUtils
     }
 
     /**
-     * Based on $baseName, this function returns all routes that match this basename..
-     * So if you pass graviton.cont.action; it will return all route names that start with the same.
-     * In our routing naming schema, this means all the routes from the same controller.
+     * Gets the controller from a Route
      *
-     * @param $baseName string basename
+     * @param Route $route Route
      *
-     * @return array array with matching routes
+     * @return bool|object The controller or false
      */
-    public function getRoutesByBasename($baseName)
+    public function getControllerFromRoute(Route $route)
     {
-        $ret = array();
-        foreach ($this->container->get('router')->getRouteCollection()->all() as $routeName => $route) {
-            if (preg_match('/^'.$baseName.'/', $routeName)) {
-                $ret[$routeName] = $route;
-            }
+        $ret = false;
+        $actionParts = explode(':', $route->getDefault('_controller'));
+
+        if (count($actionParts) == 2) {
+            $ret = $this->container->get($actionParts[0]);
         }
+
         return $ret;
     }
 }
