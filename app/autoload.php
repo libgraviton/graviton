1a8df097cd21ed6ab051894a228c2ee57d207f88
narcoticfresh <wee@xbe.ch>
2015-02-05 14:08:20 +0100

    fix regression regarding autoloader and the graviton wrapper functionality..

--- app/autoload.php
+++ app/autoload.php
@@ -4,9 +4,20 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /**
- * @var ClassLoader $loader
+ * don't touch this unless you know what you're doing..
+ * basically we have to autoloaders provided by Composer.
+ * 1) the only one when we are the main project (and we have a vendor/)
+ * 2) the one we're *we* are a dependency and we need the upper autoload.php
  */
-$loader = require __DIR__.'/../vendor/autoload.php';
+
+$primaryLoader = __DIR__.'/../vendor/autoload.php';
+$secondaryLoader = __DIR__.'/../../../autoload.php';
+
+if (file_exists($primaryLoader)) {
+    $loader = require $primaryLoader;
+} else {
+    $loader = require $secondaryLoader;
+}
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
