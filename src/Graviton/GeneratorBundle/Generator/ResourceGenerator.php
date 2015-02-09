ecb5408e524b9c3a98dbfb0f54ab06def1d14a43
Manuel Kipfer <manuel.kipfer@swisscom.com>
2015-02-09 16:36:23 +0100

    - Make ResourceGenerator testable - Test generateDocument in ResourceGenerator

--- src/Graviton/GeneratorBundle/Generator/ResourceGenerator.php
+++ src/Graviton/GeneratorBundle/Generator/ResourceGenerator.php
@@ -83,7 +83,7 @@ class ResourceGenerator extends AbstractGenerator
         $dir = $bundle->getPath();
 
         //@todo: check if the content of document is postfixed with 'Bundle' before trying to remove it.
-        $basename = substr($document, 0, -6);
+        $basename = $this->getBundleBaseName($document);
 
         $bundleNamespace = substr(get_class($bundle), 0, 0 - strlen($bundle->getName()));
 
@@ -201,7 +201,7 @@ class ResourceGenerator extends AbstractGenerator
 
         $bundleParts = explode('\\', $parameters['base']);
         $shortName = $bundleParts[0];
-        $shortBundle = substr($bundleParts[1], 0, -6);
+        $shortBundle = $this->getBundleBaseName($bundleParts[1]);
 
         $docName = implode(
             '.',
@@ -275,7 +275,7 @@ class ResourceGenerator extends AbstractGenerator
      *
      * @return \DOMDocument
      */
-    private function loadServices($dir)
+    protected function loadServices($dir)
     {
         $services = new \DOMDocument;
         $services->formatOutput = true;
@@ -294,7 +294,7 @@ class ResourceGenerator extends AbstractGenerator
      *
      * @return \DOMDocument
      */
-    private function addParam(\DOMDocument $dom, $key, $value)
+    protected function addParam(\DOMDocument $dom, $key, $value)
     {
         $paramNode = $this->addNodeIfMissing($dom, 'parameters');
 
@@ -370,7 +370,7 @@ class ResourceGenerator extends AbstractGenerator
      *
      * @return \DOMDocument
      */
-    private function addService(
+    protected function addService(
         $dom,
         $id,
         $parent = null,
