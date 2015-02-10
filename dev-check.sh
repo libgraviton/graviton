b04543215d7a04f5eef6efb12e02169ffa6e56ec
Lucas Bickel <lucas.bickel@swisscom.com>
2015-02-10 23:02:02 +0100

    add ENTB to dev-check again
    I can't recall why It wasn't active (in travis) on develop, it is on master. Also, all has been fixed
    for now.

--- dev-check.sh
+++ dev-check.sh
@@ -1,6 +1,4 @@
-# ENTB is not checked by travis so why should we? - and it's incompatible to some of the changes we made globally (@lapistrano) - uncomment to see..
-#./vendor/bin/phpcs --standard=ENTB  --ignore='src/GravitonDyn/*' --ignore='src/*/Tests/*' --ignore='app/cache/*' --ignore='app/check.php' --ignore='app/SymfonyRequirements.php' --ignore='web/check.php' --ignore='web/config.php' --ignore='app/AppCache.php' --ignore='*.css' --ignore='*.js' src/ app/ web/
-
+./vendor/bin/phpcs --standard=ENTB  --ignore='src/GravitonDyn/*' --ignore='src/*/Tests/*' --ignore='app/cache/*' --ignore='app/check.php' --ignore='app/SymfonyRequirements.php' --ignore='web/check.php' --ignore='web/config.php' --ignore='app/AppCache.php' --ignore='*.css' --ignore='*.js' src/ app/ web/
 ./vendor/bin/phpcs --standard=PSR1 --ignore='src/GravitonDyn/*' --ignore='src/*/Tests/*' --ignore='app/cache/*' --ignore='app/check.php' --ignore='app/SymfonyRequirements.php' --ignore='web/check.php' --ignore='web/config.php' --ignore='app/AppCache.php' --ignore='*.css' --ignore='*.js' src/ app/ web/
 ./vendor/bin/phpcs --standard=PSR2 --ignore='src/GravitonDyn/*' --ignore='src/*/Tests/*' --ignore='app/cache/*' --ignore='app/check.php' --ignore='app/SymfonyRequirements.php' --ignore='web/check.php' --ignore='web/config.php' --ignore='app/AppCache.php' --ignore='*.css' --ignore='*.js' src/ app/ web/
 
