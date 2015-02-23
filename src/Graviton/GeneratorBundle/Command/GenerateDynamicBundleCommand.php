ee793b98d127494cb9faea76a3bbb141e7065034
Lucas Bickel <lucas.bickel@swisscom.com>
2015-02-23 16:20:05 +0100

    implement scan strategy

--- src/Graviton/GeneratorBundle/Command/GenerateDynamicBundleCommand.php
+++ src/Graviton/GeneratorBundle/Command/GenerateDynamicBundleCommand.php
@@ -127,45 +127,6 @@ class GenerateDynamicBundleCommand extends ContainerAwareCommand
             ->load($input->getOption('json'))
         ;
 
-        // file or folder?
-        $jsonPath = $input->getOption('json');
-
-        if (is_file($jsonPath)) {
-            ;
-        } else {
-            if (is_dir($jsonPath)) {
-                ;
-            } else {
-                $output->writeln('');
-                $output->writeln('<info>No path given. Searching for "resources/definition" folders..</info>');
-                $output->writeln('');
-
-                // more broad scanning..
-                // normally, we just look in the local 'src' folder.. BUT
-                // if we find 'vendor/graviton/graviton' in our path means, we're inside a composer
-                // dependency ourselves.. in that case, search the entire vendor/ folder.. ;-)
-                // that we, we can find bundles wrapped as separate dependency..
-                $rootDir = $this->getContainer()->get('kernel')->getRootDir();
-                if (strpos($rootDir, 'vendor/graviton/graviton')) {
-                    $scanDir = dirname($this->getContainer()->get('kernel')->getRootDir()).'/../../';
-                } else {
-                    $scanDir = $input->getOption('srcDir').'../';
-                }
-
-                $findCmd = 'find '.escapeshellarg($scanDir).
-                    ' -path \'*/resources/definition*\' -iname \'*.json\'';
-
-                $findFiles = explode("\n", shell_exec($findCmd));
-
-                $filesToWorkOn = array();
-                foreach ($findFiles as $foundFile) {
-                    if (file_exists(trim($foundFile))) {
-                        $filesToWorkOn[] = trim($foundFile);
-                    }
-                }
-            }
-        }
-
         // bundles in mongodb?
         $filesToWorkOn = array_merge($filesToWorkOn, $this->getDefinitionsFromMongoDb());
 
@@ -258,7 +219,7 @@ class GenerateDynamicBundleCommand extends ContainerAwareCommand
             $arguments = array(
                 'graviton:generate:resource',
                 '--entity' => $bundleName . ':' . $thisIdName,
-                '--json' => $jsonFile,
+                '--json' => $jsonDef->getFilename(),
                 '--format' => 'xml',
                 '--fields' => $this->getFieldString($jsonDef),
                 '--with-repository' => null
@@ -319,7 +280,7 @@ class GenerateDynamicBundleCommand extends ContainerAwareCommand
             }
 
             $output->writeln('');
-            $output->writeln(sprintf('<info>Generated "%s" from file %s</info>', $bundleName, $jsonFile));
+            $output->writeln(sprintf('<info>Generated "%s" from file %s</info>', $bundleName, $jsonDef->getFilename()));
             $output->writeln('');
         }
     }
---
+++ src/Graviton/GeneratorBundle/Definition/Loader/Strategy/ScanStrategy.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * load JsonDefinition from a file
+ */
+
+namespace Graviton\GeneratorBundle\Definition\Loader\Strategy;
+
+use Graviton\GeneratorBundle\Definition\JsonDefinition;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class ScanStrategy implements StrategyInterface
+{
+    /**
+     * @var string
+     */
+    protected $scanDir;
+
+    /**
+     * @param string $scanDir dir to scan
+     *
+     * @todo this facility used to use $input->getOption('srcDir').'../' in non-vendorized mode. why?
+     */
+    public function setScanDir($scanDir)
+    {
+        // if we are vendorized we will search all vendor paths
+        if (strpos($scanDir, 'vendor/graviton/graviton')) {
+            $scanDir += '/../../';
+        }
+        echo 'using scandir '.$scanDir;
+        $this->scanDir = $scanDir;
+    }
+
+    /**
+     * may the strategy handle this input
+     *
+     * @param string|null $input input from command
+     *
+     * @return boolean
+     */
+    public function accepts($input)
+    {
+            echo 'is nulling';
+        return is_null($input);
+    }
+
+    /**
+     * load
+     *
+     * @param string|null $input input from command
+     *
+     * @return JsonDefinition[]
+     */
+    public function load($input)
+    {
+        $testMode = strpos($input, '/Tests/') !== 0;
+
+        // find json files with resources/definition in their path
+        $directory = new \RecursiveDirectoryIterator($this->scanDir);
+        $iterator = new \RecursiveIteratorIterator($directory);
+        $jsonFiles = new \RegexIterator($iterator, '/.*\/resources\/definition\/[^_].+\.json$/i', \RegexIterator::GET_MATCH);
+
+        $results = array();
+        foreach ($jsonFiles as $file) {
+            // skip files in Tests dirs (wasn't easy to do in the regex above to it's here)
+            if ($testMode || !strpos($file[0], '/Tests/')) {
+                $results[] = new JsonDefinition($file[0]);
+            }
+        }
+        return $results;
+    }
+}
