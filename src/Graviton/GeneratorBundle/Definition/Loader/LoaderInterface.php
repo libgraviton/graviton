0e213308f4718afc8df86c6933cadcbb46a463e4
Lucas Bickel <lucas.bickel@swisscom.com>
2015-02-23 13:03:04 +0100

    scaffolding for a loader to replace parts of  GenerateDynamicBundleCommand

---
+++ src/Graviton/GeneratorBundle/Definition/Loader/LoaderInterface.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * interface for getting definitions
+ */
+
+namespace Graviton\GeneratorBundle\Definition\Loader;
+
+use Graviton\GeneratorBundle\Definition\Loader\Strategy\StrategyInterface;
+use Graviton\GeneratorBundle\Definition\JsonDefinition;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+interface LoaderInterface
+{
+    /**
+     * add a strategy to the loader
+     *
+     * @param StrategyInterface $strategy strategy to add
+     *
+     * @return Loader
+     */
+    public function addStrategy(StrategyInterface $strategy);
+
+    /**
+     * @param string|null $input input from command
+     *
+     * @return JsonDefinition[]
+     */
+    public function load();
+}
