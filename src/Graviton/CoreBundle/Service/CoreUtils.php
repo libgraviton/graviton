a7bda11c2ecc4887fb1708d6b9568b49bcccc3f2
lapistano <git@bastian-feder.de>
2015-02-25 09:21:17 +0100

    Refactored GRV-1347: removed unused code

--- src/Graviton/CoreBundle/Service/CoreUtils.php
+++ src/Graviton/CoreBundle/Service/CoreUtils.php
@@ -10,27 +10,10 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://swisscom.com
+ * @link     http://swisscom.ch
  */
 class CoreUtils
 {
-    /**
-     * @var ContainerInterface service_container
-     */
-    private $container;
-
-    /**
-     * sets the container
-     *
-     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container service_container
-     *
-     * @return void
-     */
-    public function setContainer($container = null)
-    {
-        $this->container = $container;
-    }
-
     /**
      * Gets the current version we're running on..
      *
@@ -39,13 +22,14 @@ class CoreUtils
     public function getVersion($composerFile = '')
     {
         //@todo if we're in a wrapper context, use the version of the wrapper, not graviton
-        $composerFile = !empty($composerFile) ? $composerFile :__DIR__ . '/../../../../composer.json';
+        $composerFile = !empty($composerFile) ? $composerFile : __DIR__ . '/../../../../composer.json';
 
         if (file_exists($composerFile)) {
 
             $composer = json_decode(file_get_contents($composerFile), true);
 
             if (JSON_ERROR_NONE === json_last_error() && !empty($composer['version'])) {
+
                 return $composer['version'];
             } else {
                 $message = sprintf(
