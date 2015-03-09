a72a654a8a0610591b04533f5e34233a9924f443
Lucas Bickel <lucas.bickel@swisscom.com>
2015-03-09 23:27:32 +0100

    remove swagger-ui dependency and repo
    Another step at making our composer runs faster. This drops swagger-ui at the graviton level. We will be re-adding it as needed in offline instances.
    For the moment it does not break anything in those but I do plan on removing swagger-copy.sh when no one uses it anymore.

--- src/Graviton/SwaggerBundle/Resources/scripts/swagger-copy.sh
+++ src/Graviton/SwaggerBundle/Resources/scripts/swagger-copy.sh
@@ -4,6 +4,8 @@ DIR=$(cd $(dirname $0) && pwd)
 TARGETDIR=$DIR/../../../../../web/explorer
 SOURCEDIR=$DIR/../../../../../vendor/libgraviton/swagger-ui/dist
 
+echo "Warning: swagger-copy.sh is deprecated, please switch to using composer scripts to do this"
+
 if [ ! -d "$SOURCEDIR" ]; then
   SOURCEDIR=$DIR/../../../../../../../libgraviton/swagger-ui/dist
 fi
