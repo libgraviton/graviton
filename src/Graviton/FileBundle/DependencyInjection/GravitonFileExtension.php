477e758a215329e300ca564d59571a795097c341
Lucas Bickel <lucas.bickel@swisscom.com>
2015-04-29 10:56:01 +0200

    Basic generated file bundle

---
+++ src/Graviton/FileBundle/DependencyInjection/GravitonFileExtension.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * load services into di
+ */
+
+namespace Graviton\FileBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class GravitonFileExtension extends Extension
+{
+    /**
+     * load services into di
+     *
+     * @param array            $configs   config
+     * @param ContainerBuilder $container containerbuilder
+     *
+     * @return void
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.xml');
+    }
+}
