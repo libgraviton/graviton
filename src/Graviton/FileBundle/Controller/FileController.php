ae3ced52e5cd00080b35b065fda087efe68a1b5c
Lucas Bickel <lucas.bickel@swisscom.com>
2015-04-29 12:00:00 +0200

    Add basic controller
    Only scaffolding for now... I'll write test before doing anything serious.

---
+++ src/Graviton/FileBundle/Controller/FileController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * controller for gaufrette based file store
+ */
+
+namespace Graviton\FileBundle\Controller;
+
+use Graviton\RestBundle\Controller\RestController;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class FileController extends RestController
+{
+}
