2072dc492cdd9963f9b8a0220165eee5431fd8e9
Lucas Bickel <lucas.bickel@swisscom.com>
2015-04-29 15:13:28 +0200

    Config autoloading with GravitonBundleBundle

--- src/Graviton/FileBundle/DependencyInjection/GravitonFileExtension.php
+++ src/Graviton/FileBundle/DependencyInjection/GravitonFileExtension.php
@@ -5,32 +5,22 @@
 
 namespace Graviton\FileBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Graviton\BundleBundle\DependencyInjection\GravitonBundleExtension;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     http://swisscom.ch
  */
-class GravitonFileExtension extends Extension
+class GravitonFileExtension extends GravitonBundleExtension
 {
     /**
-     * load services into di
+     * {@inheritDoc}
      *
-     * @param array            $configs   config
-     * @param ContainerBuilder $container containerbuilder
-     *
-     * @return void
+     * @return string
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function getConfigDir()
     {
-        $loader = new XmlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
-        $loader->load('services.xml');
+        return __DIR__.'/../Resources/config';
     }
 }
