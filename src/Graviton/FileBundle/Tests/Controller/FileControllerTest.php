d433e35f01763e5209f1a92e9d02e7e12469c994
Lucas Bickel <lucas.bickel@swisscom.com>
2015-05-04 12:45:46 +0200

    Basic test for empty colleciton of files and test scaffolding

---
+++ src/Graviton/FileBundle/Tests/Controller/FileControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * functional test for /file
+ */
+
+namespace Graviton\FileBundle\Tests\Controller;
+
+use Graviton\TestBundle\Test\RestTestCase;
+
+/**
+ * Basic functional test for /file
+ *
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class FileControllerTest extends RestTestCase
+{
+    /**
+     * @const complete content type string expected on a resouce
+     */
+    const CONTENT_TYPE = 'application/json; charset=UTF-8; profile=http://localhost/schema/file/item';
+
+    /**
+     * @const corresponding vendorized schema mime type
+     */
+    const COLLECTION_TYPE = 'application/json; charset=UTF-8; profile=http://localhost/schema/file/collection';
+
+    /**
+     * setup client and load fixtures
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->loadFixtures(
+            array(
+                'GravitonDyn\FileBundle\DataFixtures\MongoDB\LoadFileData'
+            ),
+            null,
+            'doctrine_mongodb'
+        );
+    }
+
+    /**
+     * check for empty collections when no fixtures are loaded
+     *
+     * @return void
+     */
+    public function testFindAllEmptyCollection()
+    {
+        // reset fixtures since we already have some from setUp
+        $this->loadFixtures(array(), null, 'doctrine_mongodb');
+        $client = static::createRestClient();
+        $client->request('GET', '/file');
+
+        $response = $client->getResponse();
+        $results = $client->getResults();
+
+        $this->assertResponseContentType(self::COLLECTION_TYPE, $response);
+
+        $this->assertEquals(array(), $results);
+    }
+}
