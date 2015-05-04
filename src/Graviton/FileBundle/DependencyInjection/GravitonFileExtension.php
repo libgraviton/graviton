a60092392db9b8ffc3068f97699eb230a06b17c9
Lucas Bickel <lucas.bickel@swisscom.com>
2015-05-04 11:31:38 +0200

    Move config to parameters.yml.dist properly
    This add some indirection to the s3 config and allows us to specify it either manually during install or through cloudfoundry.
    
    We'll need to take care of letting customers that don't use s3 override this in a way that matches their use-case soonish.
    
    For now I'm implementing it like so...

--- src/Graviton/FileBundle/DependencyInjection/GravitonFileExtension.php
+++ src/Graviton/FileBundle/DependencyInjection/GravitonFileExtension.php
@@ -6,6 +6,7 @@
 namespace Graviton\FileBundle\DependencyInjection;
 
 use Graviton\BundleBundle\DependencyInjection\GravitonBundleExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
@@ -22,5 +23,41 @@ class GravitonFileExtension extends GravitonBundleExtension
     public function getConfigDir()
     {
         return __DIR__.'/../Resources/config';
+    }
+
+    /**
+     * Overwrite S3 config from cloud if available
+     *
+     * @param ContainerBuilder $container instance
+     *
+     * @return void
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        parent::prepend($container);
+
+        // populated from cf's VCAP_SERVICES variable
+        $services = $container->getParameter('vcap.services');
+        if (!empty($services)) {
+            $services = json_decode($services, true);
+            $s3 = $services['atmoss3'][0]['credentials'];
+
+            $container->setParameter('graviton.aws_s3.client.endpoint', $s3['accessHost']);
+            $container->setParameter('graviton.aws_s3.client.key', $s3['accessKey']);
+            $container->setParameter('graviton.aws_s3.client.secret', $s3['sharedSecret']);
+        } else {
+            $container->setParameter(
+                'graviton.aws_s3.client.endpoint',
+                $container->getParameter('graviton.file.s3.endpoint')
+            );
+            $container->setParameter(
+                'graviton.aws_s3.client.key',
+                $container->getParameter('graviton.file.s3.key')
+            );
+            $container->setParameter(
+                'graviton.aws_s3.client.secret',
+                $container->getParameter('graviton.file.s3.secret')
+            );
+        }
     }
 }
