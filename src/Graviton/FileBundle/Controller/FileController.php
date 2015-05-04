06a8d6ffbb24450b084ae7f421524f319606056e
Lucas Bickel <lucas.bickel@swisscom.com>
2015-05-04 11:48:43 +0200

    Scaffold integration points

--- src/Graviton/FileBundle/Controller/FileController.php
+++ src/Graviton/FileBundle/Controller/FileController.php
@@ -6,6 +6,8 @@
 namespace Graviton\FileBundle\Controller;
 
 use Graviton\RestBundle\Controller\RestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
@@ -14,4 +16,45 @@ use Graviton\RestBundle\Controller\RestController;
  */
 class FileController extends RestController
 {
+    /**
+     * Writes a new Entry to the database
+     *
+     * @param Request $request Current http request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response $response Result of action with data (if successful)
+     */
+    public function postAction(Request $request)
+    {
+        // @todo add new file to bucket and store ref in request doc
+        return parent::postaction($request);
+    }
+
+    /**
+     * Update a record
+     *
+     * @param Number  $id      ID of record
+     * @param Request $request Current http request
+     *
+     * @throws MalformedInputException
+     *
+     * @return Response $response Result of action with data (if successful)
+     */
+    public function putAction($id, Request $request)
+    {
+        // @todo update file in bucket and update ref in request document
+        return parent::putAction($id, $request);
+    }
+
+    /**
+     * Deletes a record
+     *
+     * @param Number $id ID of record
+     *
+     * @return Response $response Result of the action
+     */
+    public function deleteAction($id)
+    {
+        // @todo remove doc in bucket prior to delegating to parent
+        return parent::deleteAction($id);
+    }
 }
