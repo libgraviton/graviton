libgraviton/graviton

commit 5989b0097f834b847795ac78d58e85865394fc2c

Return raw files if non json content is requested

src/Graviton/DocumentBundle/Listener/ExtReferenceListener.php
@@ -74,6 +74,10 @@ class ExtReferenceListener
             return;
         }
 
+        if (substr(strtolower($event->getResponse()), 0, 16) !== 'application/json') {
+            return;
+        }
+
         $data = json_decode($event->getResponse()->getContent(), true);
 
         if (is_array($data) && !empty($data) && !is_string(array_keys($data)[0])) {

src/Graviton/FileBundle/Controller/FileController.php
@@ -88,6 +88,39 @@ class FileController extends RestController
     }
 
     /**
+     * respond with document if non json mime-type is requested
+     *
+     * @param Request $request Current http request
+     * @param string $id id of file
+     *
+     * @return Response
+     */
+    public function getAction(Request $request, $id)
+    {
+        if (substr(strtolower($request->headers->get('accept')), 0, 16) === 'application/json') {
+            return parent::getAction($request, $id);
+        }
+        $response = $this->getResponse();
+
+        if (!$this->gaufrette->has($id)) {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            return $response;
+        }
+
+        $record = $this->findRecord($id);
+        $data = $this->gaufrette->read($id);
+
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', $record->getMetadata()->getMime());
+
+        return $this->render(
+            'GravitonFileBundle:File:index.raw.twig',
+            ['data' => $data],
+            $response
+        );
+    }
+
+    /**
      * Deletes a record
      *
      * @param Number $id ID of record

src/Graviton/FileBundle/Resources/views/File/index.raw.twig
@@ -1,0 +1,1 @@
+{{ data|raw }}

src/Graviton/FileBundle/Tests/Controller/FileControllerTest.php
@@ -67,12 +67,13 @@ class FileControllerTest extends RestTestCase
      *
      * @return void
      */
-    public function testPostEmptyFile()
+    public function testPostFile()
     {
+        $fixtureData = file_get_contents(__DIR__.'/fixtures/test.txt');
         $client = static::createRestClient();
         $client->post(
             '/file',
-            file_get_contents(__DIR__.'/fixtures/test.txt'),
+            $fixtureData,
             [],
             [],
             ['CONTENT_TYPE' => 'text/plain'],
@@ -93,6 +94,13 @@ class FileControllerTest extends RestTestCase
         $results = $client->getResults();
 
         $this->assertEquals($link->{'$ref'}, $results->links[0]->{'$ref'});
+
+        $client = static::createClient();
+        $client->request('GET', sprintf('/file/%s', $data->id));
+
+        $results = $client->getResponse()->getContent();
+
+        $this->assertEquals($fixtureData, $results);
     }
 
     /**

src/Graviton/RestBundle/Controller/RestController.php
@@ -65,11 +65,12 @@ class RestController implements ContainerAwareInterface
     /**
      * Returns a single record
      *
-     * @param string $id ID of record
+     * @param Request $request Current http request
+     * @param string  $id      ID of record
      *
      * @return \Symfony\Component\HttpFoundation\Response $response Response with result or error
      */
-    public function getAction($id)
+    public function getAction(Request $request, $id)
     {
         $response = $this->getResponse()
             ->setStatusCode(Response::HTTP_OK);

commit fce0ab10db29a306dbeac941efdbf5fa8a756124

Fix extref mapper
This broke in 5989b0097f834b847795ac78d58e85865394fc2c by accident...

src/Graviton/DocumentBundle/Listener/ExtReferenceListener.php
@@ -74,7 +74,8 @@ class ExtReferenceListener
             return;
         }
 
-        if (substr(strtolower($event->getResponse()), 0, 16) !== 'application/json') {
+        $type = $event->getResponse()->headers->get('Content-Type');
+        if (substr(strtolower($type), 0, 16) !== 'application/json') {
             return;
         }
 