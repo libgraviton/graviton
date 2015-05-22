c88fdf817ec72e83db0ba38dd70f712e31d36c49
Lucas Bickel <lucas.bickel@swisscom.com>
2015-05-22 07:57:33 +0200

    Create simple type mapper for generator

---
+++ src/Graviton/GeneratorBundle/Generator/ResourceGenerator/FieldTypeMapper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * map fields for ResourceGenerator
+ *
+ * Use to generate corresponding serializerTypes from json-def fields.
+ */
+
+namespace Graviton\GeneratorBundle\Generator\ResourceGenerator;
+
+use Doctrine\Common\Inflector\Inflector;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class FieldTypeMapper
+{
+    /**
+     * @param array $field
+     *
+     * @return array
+     */
+    public function map($field)
+    {
+        $field['serializerType'] = $field['type'];
+        if (substr($field['type'], -2) == '[]') {
+            $field['serializerType'] = sprintf('array<%s>', substr($field['type'], 0, -2));
+        }
+
+        // @todo this assumtion is a hack and needs fixing
+        if ($field['type'] === 'array') {
+            $field['serializerType'] = 'array<string>';
+        }
+
+        if ($field['type'] === 'object') {
+            $field['serializerType'] = 'array';
+        }
+
+        return $field;
+    }
+}
