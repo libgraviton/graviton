cb0dac6e1a88934e369cc139bacaf94dded8ac5e
Lucas Bickel <lucas.bickel@swisscom.com>
2015-05-30 12:15:49 +0200

    Grab collection name from json-def if available

--- src/Graviton/GeneratorBundle/Generator/ResourceGenerator.php
+++ src/Graviton/GeneratorBundle/Generator/ResourceGenerator.php
@@ -559,6 +559,9 @@ class ResourceGenerator extends AbstractGenerator
 
                 // get stuff from json definition
                 if ($this->json instanceof JsonDefinition) {
+                    // id is also name of collection in mongodb
+                    $this->addAttributeToNode('collection', $this->json->getId(), $dom, $tagNode);
+
                     // is this read only?
                     if ($this->json->isReadOnlyService()) {
                         $this->addAttributeToNode('read-only', 'true', $dom, $tagNode);
