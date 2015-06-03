ddc32271e40baba56afbc88343bdc553e2c1a990
Lucas Bickel <lucas.bickel@swisscom.com>
2015-06-03 10:59:10 +0200

    scaffold form builder

---
+++ src/Graviton/DocumentBundle/Form/Type/DocumentType.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * generic form builder that grabs data from doctrine/serializer/json and builds forms
+ */
+
+namespace Graviton\DocumentBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class DocumentType extends AbstractType
+{
+}
---
+++ src/Graviton/DocumentBundle/Tests/Form/Type/DocumentTypeTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * test generic form builder class
+ */
+
+namespace Graviton\DocumentBundle\Tests\Form\Type;
+
+use Graviton\DocumentBundle\Form\Type\DocumentType;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class DocumentTypeTest extends \PHPUnit_Framework_TestCase
+{
+}
