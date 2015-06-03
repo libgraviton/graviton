719a5578e00f520ef9a4210cf06c9f20beca74b5
narcoticfresh <wee@xbe.ch>
2015-06-03 08:06:38 +0200

    first add of the oauth client bundle..

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -73,6 +73,7 @@ class AppKernel extends Kernel
             new \Graviton\RqlParserBundle\GravitonRqlParserBundle(),
             new \Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new \Eo\AirbrakeBundle\EoAirbrakeBundle(),
+            new \HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
