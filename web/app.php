7b64804f40018d2e23defd90f81f6a07ac5a169d
narcoticfresh <wee@xbe.ch>
2015-06-08 11:16:51 +0200

    remove env stuff to app.php; get rid of app_env. change path in nginx cloud config again..

--- web/app.php
+++ web/app.php
@@ -1,27 +1,51 @@
 <?php
 /**
- * main entrypoint for graviton (forced production)
+ * main entrypoint for graviton; env dependent
  *
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license  http://opensource.org/licenses/GPL GPL
  * @link     http://swisscom.ch
  */
 
-//use Symfony\Component\ClassLoader\ApcClassLoader;
+use Graviton\AppKernel;
+use Graviton\BundleBundle\GravitonBundleBundle;
+use Graviton\BundleBundle\Loader\BundleLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 // @codingStandardsIgnoreStart
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // @codingStandardsIgnoreEnd
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+require_once __DIR__.'/../app/AppKernel.php';
 
-putenv('SYMFONY_ENV=prod');
-require_once 'app_env.php';
+// check for env
+$env = getenv('SYMFONY_ENV');
+if (false === $env) {
+    // fallback to 'prod'
+    $env = 'prod';
+}
+
+$activateDebug = false;
+if (strpos($env, 'dev') !== false) {
+    // catch also oauth_dev & co..
+    $activateDebug = true;
+}
+
+if ($activateDebug) {
+    Debug::enable();
+}
+$kernel = new AppKernel($env, $activateDebug);
+
+$kernel->setBundleLoader(new BundleLoader(new GravitonBundleBundle()));
+$kernel->loadClassCache();
+
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller
+// instead of relying on the configuration parameter
+// Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
