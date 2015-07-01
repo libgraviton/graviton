38fcd155a26f31f17c81584847b84f719a7773ad
narcoticfresh <wee@xbe.ch>
2015-07-01 11:25:28 +0200

    remove unused imports..

--- src/Graviton/SwaggerBundle/Command/SwaggerGenerateCommand.php
+++ src/Graviton/SwaggerBundle/Command/SwaggerGenerateCommand.php
@@ -7,11 +7,8 @@ namespace Graviton\SwaggerBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Process\Process;
 
 /**
  * Generates swagger.json
