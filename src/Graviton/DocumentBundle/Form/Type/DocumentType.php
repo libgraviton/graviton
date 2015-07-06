7f11f0237b8013d11b9868cc20ca0233d2cb7dc3
Lucas Bickel <lucas.bickel@swisscom.com>
2015-07-06 19:05:29 +0200

    Remove whoopsie-daisy
    This should not have contained any debug code ;)

--- src/Graviton/DocumentBundle/Form/Type/DocumentType.php
+++ src/Graviton/DocumentBundle/Form/Type/DocumentType.php
@@ -70,10 +70,8 @@ class DocumentType extends AbstractType
                 $type = clone $this;
                 $type->initialize($options['data_class']);
             } elseif ($type === 'date' || $type == 'datetime') {
-                echo 'got form type and making string of it';
                 $options['widget'] = 'single_text';
                 $options['input'] = 'string';
-                $options['invalid_message'] = 'lerrooooooor';
             } elseif ($type == 'collection' && $options['type'] == 'form') {
                 $subType = clone $this;
                 $subType->initialize($options['options']['data_class']);
