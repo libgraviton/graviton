3c54c6d257c2e1a700c047949e58eec5951f6413
narcoticfresh <wee@xbe.ch>
2015-07-13 14:44:24 +0200

    add first 2 tests for showcase..

--- src/Graviton/CoreBundle/Tests/Controller/ShowcaseControllerTest.php
+++ src/Graviton/CoreBundle/Tests/Controller/ShowcaseControllerTest.php
@@ -36,27 +36,67 @@ class ShowcaseControllerTest extends RestTestCase
     }
 
     /**
-     * check if all fixtures are returned on GET
+     * See how our missing fields are explained to us
      *
      * @return void
      */
-    public function testAllFeatures()
+    public function testMissingFields()
     {
-        $fullDocument = json_decode(
-            file_get_contents(dirname(__FILE__).'/../resources/showcase-complete.json'),
+        $document = json_decode(
+            file_get_contents(dirname(__FILE__).'/../resources/showcase-incomplete.json'),
             true
         );
 
         $client = static::createRestClient();
+        $client->post('/hans/showcase', $document);
 
+        $expectedErrors = [];
+        $expectedErrors[0] = new \stdClass();
+        $expectedErrors[0]->property_path = "children[someOtherField]";
+        $expectedErrors[0]->message = "This value is not valid.";
+        $expectedErrors[1] = new \stdClass();
+        $expectedErrors[1]->property_path = "data.contact.type";
+        $expectedErrors[1]->message = "This value should not be blank.";
+        $expectedErrors[2] = new \stdClass();
+        $expectedErrors[2]->property_path = "data.contact.protocol";
+        $expectedErrors[2]->message = "This value should not be blank.";
+        $expectedErrors[3] = new \stdClass();
+        $expectedErrors[3]->property_path = "data.contact.value";
+        $expectedErrors[3]->message = "This value should not be blank.";
+        $expectedErrors[4] = new \stdClass();
+        $expectedErrors[4]->property_path = "data.contactCode.text";
+        $expectedErrors[4]->message = "This value should not be blank.";
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($expectedErrors),
+            json_encode($client->getResults())
+        );
+    }
+
+    /**
+     * check if all fixtures are returned on GET
+     *
+     * @return void
+     */
+    public function testMinimalPost()
+    {
+        $fullDocument = json_decode(
+            file_get_contents(dirname(__FILE__).'/../resources/showcase-minimal.json'),
+            true
+        );
+
+        $client = static::createRestClient();
         $client->post('/hans/showcase', $fullDocument);
         $response = $client->getResponse();
 
-        //var_dump($response); die;
-        //$results = $client->getResults();
+        $client = static::createRestClient();
+        $client->request('GET', $response->headers->get('Location'));
 
-        //
-        //var_dump($client);
+        $response = $client->getResults();
 
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($fullDocument),
+            json_encode($response)
+        );
     }
 }
