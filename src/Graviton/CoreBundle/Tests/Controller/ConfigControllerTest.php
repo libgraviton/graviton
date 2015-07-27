76c1e6b74865fb97cba407b2c177cf696342732e
narcoticfresh <wee@xbe.ch>
2015-07-27 16:58:09 +0200

    more assertions to see if Link header is correctly encoded..

--- src/Graviton/CoreBundle/Tests/Controller/ConfigControllerTest.php
+++ src/Graviton/CoreBundle/Tests/Controller/ConfigControllerTest.php
@@ -37,17 +37,60 @@ class ConfigControllerTest extends RestTestCase
      * We need to make sure that our Link headers are properly encoded for our RQL parser.
      * This test tries to ensure that as we have resources named-like-this in /core/config.
      *
+     * @param string $expression  expression
+     * @param int    $resultCount expected res count
+     *
+     * @dataProvider rqlCheckDataProvider
+     *
      * @return void
      */
-    public function testLinkHeaderEncoding()
+    public function testLinkHeaderEncodingDash($expression, $resultCount)
     {
-        $correctlyEncodedString = 'tablet%252Dhello%252Dmessage';
-
+        $expression = urlencode($expression);
         $client = static::createRestClient();
-        $client->request('GET', '/core/config?q=eq(id,'.$correctlyEncodedString.')');
+        $client->request('GET', '/core/config?q='.$expression);
         $response = $client->getResponse();
 
-        $this->assertContains($correctlyEncodedString, $response->headers->get('Link'));
-        $this->assertEquals(1, count($client->getResults()));
+        $this->assertContains($expression, $response->headers->get('Link'));
+        $this->assertEquals($resultCount, count($client->getResults()));
+    }
+
+    /**
+     * Data provider for self-Link-header check
+     *
+     * @return array data
+     */
+    public function rqlCheckDataProvider()
+    {
+        return array(
+            array(
+                'eq(id,'.$this->encodeString('tablet-hello-message').')',
+                1
+            ),
+            array(
+                'eq(id,'.$this->encodeString('admin-additional+setting').')',
+                1
+            ),
+            array(
+                'like(key,'.$this->encodeString('hello-').'*)',
+                1
+            )
+        );
+    }
+
+    /**
+     * Encodes our expressions
+     *
+     * @param string $value value
+     *
+     * @return string encoded value
+     */
+    private function encodeString($value)
+    {
+        return str_replace(
+            array('-', '_', '.', '~'),
+            array('%2D', '%5F', '%2E', '%7E'),
+            $value
+        );
     }
 }
