52d8fb3291a29d96448465191031b8c50c2bfa8f
narcoticfresh <wee@xbe.ch>
2015-08-04 12:28:10 +0200

    create class stubs..

---
+++ src/Graviton/DocumentBundle/Tests/DependencyInjection/CompilerPass/Resources/Document/A.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Graviton\DocumentBundle\Tests\DependencyInjection\CompilerPass\Resources\Document;
+
+use Graviton\I18nBundle\Document\TranslatableDocumentInterface;
+
+class A implements TranslatableDocumentInterface {
+
+    /**
+     * return all translatable fields
+     *
+     * @return string[]
+     */
+    public function getTranslatableFields() {
+        return array('title');
+    }
+
+    /**
+     * return all pretranslated fields
+     *
+     * @return string[]
+     */
+    public function getPreTranslatedFields() {
+        return array();
+    }
+}
