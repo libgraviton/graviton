08dc3f21f7404f66aec002497756bc1b1e7dd358
aevtushenko <aevtushenko@xiag.ch>
2015-08-18 10:13:46 +0600

    Add serializer handler for ArrayObject.

---
+++ src/Graviton/GeneratorBundle/Serializer/Handler/ArrayObjectHandler.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * ArrayObjectHandler class file
+ */
+
+namespace Graviton\GeneratorBundle\Serializer\Handler;
+
+use JMS\Serializer\Context;
+use JMS\Serializer\VisitorInterface;
+
+/**
+ * ArrayObject handler
+ *
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class ArrayObjectHandler
+{
+    /**
+     * Serialize ArrayObject
+     *
+     * @param VisitorInterface $visitor Visitor
+     * @param \ArrayObject     $data    Data
+     * @param array            $type    Type
+     * @param Context          $context Context
+     * @return \ArrayObject
+     */
+    public function serializeArrayObjectToJson(VisitorInterface $visitor, \ArrayObject $data, array $type, Context $context)
+    {
+        $type['name'] = 'array';
+        return new \ArrayObject($visitor->visitArray($data->getArrayCopy(), $type, $context));
+    }
+
+    /**
+     * Deserialize ArrayObject
+     *
+     * @param VisitorInterface $visitor Visitor
+     * @param array            $data    Data
+     * @param array            $type    Type
+     * @param Context          $context Context
+     * @return \ArrayObject
+     */
+    public function deserializeArrayObjectFromJson(VisitorInterface $visitor, array $data, array $type, Context $context)
+    {
+        $type['name'] = 'array';
+        return new \ArrayObject($visitor->visitArray($data, $type, $context));
+    }
+}
