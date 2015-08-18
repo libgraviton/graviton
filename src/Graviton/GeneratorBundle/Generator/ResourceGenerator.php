8f47c52d94ba37480a46077faf60c5dce2c18c99
Samuel Ackermann <samuel.ackermann@swisscom.com>
2015-08-18 11:40:17 +0200

    add a new param for twig

--- src/Graviton/GeneratorBundle/Generator/ResourceGenerator.php
+++ src/Graviton/GeneratorBundle/Generator/ResourceGenerator.php
@@ -163,6 +163,7 @@ class ResourceGenerator extends AbstractGenerator
             ->setParameter('json', $this->json)
             ->setParameter('fields', $fields)
             ->setParameter('basename', $basename)
+            ->setParameter('recordOriginModifiable', $this->json->isRecordOriginModifiable())
             ->getParameters();
 
         $this->generateDocument($parameters, $dir, $document, $withRepository);
