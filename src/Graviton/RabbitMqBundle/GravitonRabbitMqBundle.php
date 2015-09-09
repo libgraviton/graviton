1c15bda0bb7cd98f67b5db730dcfc1b286c4022e
narcoticfresh <wee@xbe.ch>
2015-09-09 12:37:28 +0200

    correct documentation..

--- src/Graviton/RabbitMqBundle/GravitonRabbitMqBundle.php
+++ src/Graviton/RabbitMqBundle/GravitonRabbitMqBundle.php
@@ -1,24 +1,17 @@
 <?php
 /**
- * core infrastructure like logging and framework.
+ * rabbitmq based messaging bundle
  */
 
 namespace Graviton\RabbitMqBundle;
 
 use Graviton\BundleBundle\GravitonBundleInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * GravitonCoreBundle
- *
- * WARNING: Don't change me without changing Graviton\GeneratorBundle\Manipulator\BundleBundleManipulator
- *
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     http://swisscom.ch
- *
- * @see \Graviton\GeneratorBundle\Manipulator\BundleBundleManipulator
  */
 class GravitonRabbitMqBundle extends Bundle implements GravitonBundleInterface
 {
@@ -31,8 +24,6 @@ class GravitonRabbitMqBundle extends Bundle implements GravitonBundleInterface
      */
     public function getBundles()
     {
-        return array(
-
-        );
+        return [];
     }
 }
