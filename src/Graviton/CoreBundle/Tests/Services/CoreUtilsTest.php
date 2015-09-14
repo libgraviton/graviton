0de33e0f1860843e7a509020d9db95a73e357a7b
Jan Friedli <jan.friedli@gmx.ch>
2015-09-14 11:31:05 +0200

    added core utils tests$

--- src/Graviton/CoreBundle/Tests/Services/CoreUtilsTest.php
+++ src/Graviton/CoreBundle/Tests/Services/CoreUtilsTest.php
@@ -14,43 +14,48 @@ namespace Graviton\CoreBundle\Service;
  */
 class CoreUtilsTest extends \PHPUnit_Framework_TestCase
 {
+    private $versions = array(
+                array("id" => "graviton", "version" => "0.25.1", "isWrapper" => true),
+                array("id" => "financing", "version" => "0.1", "isWrapper" => false),
+            );
     /**
      * Verifies the correct behavior of getVersion()
      *
-     * @dataProvider versionAndFileProvider
-     *
-     * @param array $version Version array to be used
-     *
      * @return void
      */
-    public function testGetVersion($version)
+    public function testGetVersionById()
     {
-        $utils = new CoreUtils($version);
-        $this->assertEquals($version[0]['version'], $utils->getVersionById('graviton')['version']);
+        $utils = new CoreUtils($this->versions);
+        $this->assertEquals($this->versions[0]['version'], $utils->getVersionById('graviton')['version']);
     }
 
     /**
-     * Provides test sets for the getVersion() test.
+     * Verifies the correct behavior of getWrapperVersion()
      *
-     * @return array
+     * @return void
      */
-    public function versionAndFileProvider()
+    public function testGetWrapperVersion()
     {
-        return array(
-            'get from default file' => array(array(array("id" => "graviton", "version" => "0.25.1"))),
+        $utils = new CoreUtils($this->versions);
+        $this->assertEquals(
+            array("id" => "graviton", "version" => "0.25.1", "isWrapper" => true),
+            $utils->getWrapperVersion()
         );
+        $this->assertInternalType('array', $utils->getWrapperVersion());
     }
-    //
-    //    /**
-    //     * Verifies the correct behavior of getVersion()
-    //     *
-    //     * @return void
-    //     */
-    //    public function testGetDefaultVersion()
-    //    {
-    //        $utils = new CoreUtils(array(array("id" => "graviton", "version" => "0.25.1")));
-    //
-    //        $this->setExpectedException('\Graviton\ExceptionBundle\Exception\MissingVersionFileException');
-    //        $utils->getVersion(__DIR__ . '/../fixtures/invalid_composer.json');
-    //    }
+
+    /**
+     * Verifies the correct behavior of getVersionInHeaderFormat()
+     *
+     * @return void
+     */
+    public function testGetVersionInHeaderFormat()
+    {
+        $utils = new CoreUtils($this->versions);
+        $this->assertEquals(
+            'graviton: 0.25.1; financing: 0.1; ',
+            $utils->getVersionInHeaderFormat()
+        );
+        $this->assertInternalType('string', $utils->getVersionInHeaderFormat());
+    }
 }
