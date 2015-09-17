3e95ad7f849d78e8ea364153ce2bc77113750a40
Jan Friedli <jan.friedli@gmx.ch>
2015-09-17 10:19:22 +0200

    refactored shell_exec simplified logic

--- src/Graviton/CoreBundle/Compiler/VersionCompilerPass.php
+++ src/Graviton/CoreBundle/Compiler/VersionCompilerPass.php
@@ -5,7 +5,9 @@ namespace Graviton\CoreBundle\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Container;
+use \Symfony\Component\Yaml\Parser;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
@@ -16,6 +18,16 @@ class VersionCompilerPass implements CompilerPassInterface
 {
 
     /**
+     * @var rootDir
+     */
+    private $rootDir;
+
+    /**
+     * @var config
+     */
+    private $config;
+
+    /**
      * add version numbers of packages to the container
      *
      * @param ContainerBuilder $container Container
@@ -24,22 +36,29 @@ class VersionCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
+        $this->rootDir = $container->getParameter('kernel.root_dir');
+        $this->config = $this->getVersionConfig();
         $container->setParameter(
             'graviton.core.version.data',
-            $this->getPackageVersions($container->getParameter('kernel.root_dir'))
+            $this->getPackageVersions()
         );
     }
 
     /**
-     * @param string $rootDir path to root dir
+     * gets all versions
      *
      * @return array version numbers of packages
      */
-    private function getPackageVersions($rootDir)
+    private function getPackageVersions()
     {
-        $versions = array();
-        array_push($versions, $this->getContextVersion($rootDir));
-        $versions = $this->getInstalledPackagesVersion($rootDir, $versions);
+        if ($this->isDesiredVersion('self')) {
+            $versions = [
+                $this->getContextVersion(),
+            ];
+        } else {
+            $versions = array();
+        }
+        $versions = $this->getInstalledPackagesVersion($versions);
 
         return $versions;
     }
@@ -47,18 +66,12 @@ class VersionCompilerPass implements CompilerPassInterface
     /**
      * returns the version of graviton or wrapper
      *
-     * @param string $rootDir path to root dir
      * @return array
      */
-    private function getContextVersion($rootDir)
+    private function getContextVersion()
     {
-        if (strpos($rootDir, 'vendor')) {
-            //@todo shell exec needs to be refactored
-            $result = shell_exec('cd ' . escapeshellarg($rootDir) . '/../../../../  && composer show -s --no-ansi');
-        } else {
-            $result = shell_exec('composer show -s --no-ansi');
-        }
-        $lines = explode(PHP_EOL, $result);
+        $output = $this->runCommandInContext('composer show -s --no-ansi', $this->rootDir);
+        $lines = explode(PHP_EOL, $output);
         $wrapper = array();
         foreach ($lines as $line) {
             if (strpos($line, 'versions') !== false) {
@@ -74,28 +87,99 @@ class VersionCompilerPass implements CompilerPassInterface
     /**
      * returns version for every installed package
      *
-     * @param string $rootDir  path to root directory
-     * @param array  $versions versions array
+     * @param array $versions versions array
      * @return array
      */
-    private function getInstalledPackagesVersion($rootDir, $versions)
+    private function getInstalledPackagesVersion($versions)
     {
-        if (strpos($rootDir, 'vendor')) {
-            $command = 'cd ' . escapeshellarg($rootDir) . '/../../../../ && composer show -i';
-            $packageNames = shell_exec($command);
-        } else {
-            $packageNames = shell_exec('composer show -i');
-        }
-        $packages = explode(PHP_EOL, $packageNames);
+        $output = $this->runCommandInContext('composer show -i');
+
+        $packages = explode(PHP_EOL, $output);
         //last index is always empty
         array_pop($packages);
+
         foreach ($packages as $package) {
-            preg_match_all('/([^\s]+)/', $package, $match);
-            if ((strpos($match[0][0], 'grv') === 0 | $match[0][0] === 'graviton') && !empty($match[0][1])) {
-                array_push($versions, array('id' => $match[0][0], 'version' => $match[0][1]));
+            $content = preg_split('/([\s]+)/', $package);
+            if ($this->isDesiredVersion($content[0])) {
+                array_push($versions, array('id' => $content[0], 'version' => $content[1]));
             }
         }
 
         return $versions;
+    }
+
+    /**
+     * runs a bash/shell command depending on the context
+     *
+     * @param string $command shell/bash command
+     * @return string
+     */
+    private function runCommandInContext($command)
+    {
+        if ($this->isWrapperContext()) {
+            $process = new Process('cd ' . escapeshellarg($this->rootDir) . '/../../../../  && ' . $command);
+        } else {
+            $process = new Process('cd ' . escapeshellarg($this->rootDir) . '/../ && ' . $command);
+        }
+
+        try {
+            $process->mustRun();
+
+            return $process->getOutput();
+        } catch (ProcessFailedException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * checks if the package version is configured
+     *
+     * @param string $packageName package name
+     * @return boolean
+     */
+    private function isDesiredVersion($packageName)
+    {
+        foreach ($this->config['desiredVersions'] as $confEntry) {
+            if ($confEntry == $packageName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * read and parses version config file
+     *
+     * @return array
+     */
+    private function getVersionConfig()
+    {
+        $parser = new Parser();
+        if ($this->isWrapperContext()) {
+            $parsedConfig = $parser->parse(
+                file_get_contents($this->rootDir . '/../../../../app/config/version_service.yml')
+            );
+        } else {
+            $parsedConfig = $parser->parse(
+                file_get_contents($this->rootDir . '/config/version_service.yml')
+            );
+        }
+
+        return $parsedConfig;
+    }
+
+    /**
+     * checks if context is a wrapper or not
+     *
+     * @return boolean
+     */
+    private function isWrapperContext()
+    {
+        if (strpos($this->rootDir, 'vendor')) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
