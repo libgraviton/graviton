39075e4fef7a921ca1fe13dc78d6826c29649f06
Jan Friedli <jan.friedli@gmx.ch>
2015-09-22 14:03:14 +0200

    handle config in wrapper context

--- src/Graviton/CoreBundle/Compiler/VersionCompilerPass.php
+++ src/Graviton/CoreBundle/Compiler/VersionCompilerPass.php
@@ -156,9 +156,15 @@ class VersionCompilerPass implements CompilerPassInterface
     private function getVersionConfig()
     {
         $parser = new Parser();
-        $parsedConfig = $parser->parse(
-            file_get_contents($this->rootDir . '/config/version_service.yml')
-        );
+        if ($this->isWrapperContext()) {
+            $parsedConfig = $parser->parse(
+                file_get_contents($this->rootDir . '/../../../../app/config/version_service.yml')
+            );
+        } else {
+            $parsedConfig = $parser->parse(
+                file_get_contents($this->rootDir . '/config/version_service.yml')
+            );
+        }
 
         return $parsedConfig;
     }
