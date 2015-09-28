a95d87fb5d73765ae0986ea49e3bb2bf51f1a349
narcoticfresh <wee@xbe.ch>
2015-09-28 13:08:52 +0200

    add new validator to tests

--- src/Graviton/RabbitMqBundle/Tests/Controller/EventStatusControllerTest.php
+++ src/Graviton/RabbitMqBundle/Tests/Controller/EventStatusControllerTest.php
@@ -60,4 +60,53 @@ class EventStatusControllerTest extends RestTestCase
         $this->assertEquals("data.status[0].status", $results[0]->propertyPath);
         $this->assertContains("\"thinking\" is not a valid status string", $results[0]->message);
     }
+
+    /**
+     * test to see if we can insert a status and if graviton complains about an invalid status
+     *
+     * @return void
+     */
+    public function testInvalidInformationType()
+    {
+        $status = new \stdClass();
+        $status->id = 'mynewstatus';
+        $status->createDate = '2015-09-24T07:21:24+0000';
+        $status->eventName = 'document.test.app.create';
+
+        $informationEntry = new \stdClass();
+        $informationEntry->workerId = 'testworker';
+        $informationEntry->type = 'info';
+        $informationEntry->content = 'see the attached document';
+        $informationEntry->{'$ref'} = 'http://localhost/core/app/admin';
+
+        $status->information = [$informationEntry];
+
+        $client = static::createRestClient();
+        $client->put('/event/status/mynewstatus', $status);
+
+        $this->assertNull($client->getResults());
+        $this->assertNull($client->getResponse()->headers->get('Location'));
+        $this->assertEquals(204, $client->getResponse()->getStatusCode());
+
+        // get our object again
+        $client = static::createRestClient();
+        $client->request('GET', '/event/status/mynewstatus');
+        $results = $client->getResults();
+
+        $this->assertEquals('testworker', $results->information[0]->workerId);
+        $this->assertEquals('info', $results->information[0]->type);
+        $this->assertEquals('see the attached document', $results->information[0]->content);
+        $this->assertEquals('http://localhost/core/app/admin', $results->information[0]->{'$ref'});
+
+        // set invalid information type
+        $results->information[0]->type = 'bogus';
+
+        $client = static::createRestClient();
+        $client->put('/event/status/mynewstatus', $results);
+        $results = $client->getResults();
+
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertEquals("data.information[0].type", $results[0]->propertyPath);
+        $this->assertContains("\"bogus\" is not a valid information type", $results[0]->message);
+    }
 }
