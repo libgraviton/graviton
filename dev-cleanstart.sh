9e3d4657fe37cf02ca6e38819e6a4ac4e39adc77
narcoticfresh <wee@xbe.ch>
2015-10-08 15:24:44 +0200

    re-bump after develop merge

--- dev-cleanstart.sh
+++ dev-cleanstart.sh
@@ -1,4 +1,4 @@
 rm -Rf app/cache app/logs app/bootstrap.php.cache
 composer install
-php app/console doctrine:mongodb:fixtures:load
+#php app/console doctrine:mongodb:fixtures:load
 php app/console server:run
