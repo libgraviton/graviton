d3aa8c2e9ba98bd68f436738451550715a0694bc
narcoticfresh <wee@xbe.ch>
2015-10-15 13:57:16 +0200

    add doc to cache utils; cleanup..

--- src/Graviton/I18nBundle/Service/I18nCacheUtils.php
+++ src/Graviton/I18nBundle/Service/I18nCacheUtils.php
@@ -1,69 +1,116 @@
 <?php
 /**
- * service for i18n stuff
+ * service for i18n stuff relating to symfony translation loader and its caching.
+ *
+ * it basically receives the 'resource_files' array from a Translator (that is built statically in the Container)
+ * and adds runtime elements to it. behind the scenes, it invalidates translator caches and creates the resource files.
+ * all with the idea that new 'translation domains' get added at runtime without the need of a container rebuild.
+ * only then it will be actually loaded by the translation loaders and end up in the final catalogue.
+ * caches are in place to optimize the performance impact.
  */
 
 namespace Graviton\I18nBundle\Service;
 
 use Doctrine\Common\Cache\FilesystemCache;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 /**
- * A service (meaning symfony service) providing some convenience stuff when dealing with our RestController
- * based services (meaning rest services).
- *
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     http://swisscom.ch
  */
 class I18nCacheUtils
 {
+    /**
+     * full path to translations cache
+     *
+     * @var string
+     */
+    private $cacheDirTranslations;
 
     /**
+     * our cache file for doctrien cache
+     *
      * @var string
      */
     private $cacheFile;
 
     /**
+     * the cache key we use for our addition array
+     *
      * @var string
      */
     private $cacheKey;
 
     /**
+     * the cache key we use for the full resource map
+     *
+     * @var string
+     */
+    private $cacheKeyFinalResource;
+
+    /**
+     * doctrine filesystemcache
+     *
+     * @var FilesystemCache
+     */
+    private $cache;
+
+    /**
+     * the loader id suffix (like 'odm')
+     *
      * @var string
      */
     private $loaderId;
 
     /**
+     * full path to the bundle resource dir
+     *
      * @var string
      */
     private $resourceDir;
 
     /**
-     * @var FilesystemCache
+     * this map contains all added resources (added to all translator knows already)
+     *
+     * @var array
      */
-    private $cache;
-
     private $addedResources = array();
 
+    /**
+     * if the postpersistListener invalidates something, it will be put here
+     *
+     * @var array
+     */
+    private $invalidations = array();
+
+    /**
+     * a boolean flag telling us if a new map persist is necessary
+     * (that is, when some new addition has been added that needs a resource map regeneration)
+     *
+     * @var boolean
+     */
     private $isDirty = false;
 
     /**
      * Constructor
      *
-     * @param string              $defaultLanguage    default language
+     * @param string $cacheDir full path to cache dir
+     * @param string $loaderId loader id suffix
      */
     public function __construct(
         $cacheDir,
         $loaderId
     ) {
         // caching
+        $this->cacheDirTranslations = $cacheDir . '/translations';
         $this->cacheFile = $cacheDir . '/addedI18nResources';
         $this->cache = new FilesystemCache($this->cacheFile);
 
         // cache keys
         $this->cacheKey = 'addedTranslations';
         $this->cacheKeyFinalResource = 'finalResources';
-        $this->cacheKeyFinalResourceHash = 'finalResourceHash';
 
         $this->loaderId = $loaderId;
         $this->resourceDir = __DIR__.'/../Resources/translations/';
@@ -74,6 +121,59 @@ class I18nCacheUtils
         }
     }
 
+    /**
+     * this shall be called by a Translator.
+     * it adds our additions to the already existent ones in the Translator and returns it.
+     * as this is called quite often, we cache the final result (the full map including the translator resources)
+     * and only regenerate that when a *new* domain has been added. basic invalidations by the PostPersistListener
+     * will *not* result in a rebuild here - only if a new domain has been added.
+     *
+     * @param array $resources the resources array of the translator
+     *
+     * @return array the finalized map containing translator resources and our additions
+     */
+    public function getResources($resources)
+    {
+        $finalResources = null;
+
+        // do we have a full resource map in the cache already? (full = translator + additions)
+        if ($this->cache->contains($this->cacheKeyFinalResource)) {
+            $finalResources = $this->cache->fetch($this->cacheKeyFinalResource);
+        }
+
+        // do we have cached additions?
+        if (!is_array($finalResources) && $this->cache->contains($this->cacheKey)) {
+            // merge the two together, always keep an eye to not duplicate (paths are different!)
+            $finalResources = $this->mergeResourcesWithAdditions($resources);
+
+            // cache it
+            $this->cache->save($this->cacheKeyFinalResource, $finalResources);
+        }
+
+        // so, did we got anything?
+        if (is_array($finalResources)) {
+            $resources = $finalResources;
+        }
+
+        return $resources;
+    }
+
+    /**
+     * shall be called by the PostPersistTranslatorListener (or other interested parties).
+     * if someone invalidates a locale & domain pair, this will lead to:
+     * - removal of the symfony translation cache files
+     * (if this pair has never been seen)
+     * - creation of the resource files ("trigger files")
+     * - a regeneration of the full resource map for the translator
+     *
+     * please note that calling invalidate() will do the above mentioned in a lazy way
+     * on __destruct() of this object.
+     *
+     * @param string $locale locale (de,en)
+     * @param string $domain the domain
+     *
+     * @return void
+     */
     public function invalidate($locale, $domain)
     {
         $filename = sprintf('%s.%s.%s', $domain, $locale, $this->loaderId);
@@ -82,93 +182,108 @@ class I18nCacheUtils
             $this->addedResources[$locale][] = $filename;
             $this->isDirty = true;
         }
+
+        $this->invalidations[$locale] = '';
     }
 
+    /**
+     * sets the resource dir
+     *
+     * @param string $dir resource dir
+     *
+     * @return void
+     */
     public function setResourceDir($dir)
     {
         $this->resourceDir = $dir;
     }
 
-    public function getResources($resources)
+    /**
+     * Merges the cached additions with the one the Translator already has.
+     * I need to use preg_grep() here as I'm unable to compose an absolute path that is
+     * identical to the one the Translator would have already as I have to deal with relative
+     * paths here (and rightfully so). This shouldn't hurt too much as the end result is cached
+     * and only redone if something changes.
+     *
+     * @param array $resources resources
+     *
+     * @return array finalized full map
+     */
+    private function mergeResourcesWithAdditions($resources)
     {
-        $resourceHash = sha1(serialize($resources));
+        foreach ($this->addedResources as $locale => $files) {
+            foreach ($files as $file) {
+                $hits = preg_grep('/\/'.str_replace('.', '\\.', $file).'$/', $resources[$locale]);
+                if (count($hits) === 0) {
+                    $resourceFile = $this->resourceDir.$file;
 
-        $finalResources = null;
-        if ($this->resourceIsInCache($resourceHash)) {
-            $finalResources = $this->cache->fetch(cacheKeyResource);
+                    // make sure the file exists
+                    $fs = new Filesystem();
+                    if (!$fs->exists($resourceFile)) {
+                        $fs->touch($resourceFile);
+                    }
+
+                    $resources[$locale][] = $resourceFile;
+                }
+            }
         }
-
-        if (!is_array($finalResources) && $this->cache->contains($this->cacheKey)) {
-            // merge the two together, always keep an eye to not duplicate (paths are different!)
-            $finalResources = $this->mergeResourcesWithAdditions($resources);
-
-            //echo "hans"; die;
-        }
-
-        /*
-        var_dump($resourceHash);
-        var_dump($resources);
-
-
-        $added = array();
-        if ($this->cache->contains($this->cacheKey)) {
-            $added = $this->cache->fetch($this->cacheKey);
-        }
-
-        var_dump($added); die;
-        */
-
         return $resources;
     }
 
     /**
-     * Merges the cached additions with the one the Translator already has.
-     * I need to use preg_grep() here as I'm unable to compose an absolute path that is
-     * exactly like the one the Translator would have already as I have to deal with relative
-     * paths here (rightfully so). This shouldn't hurt too much as the end result is cache too
-     * and only redone if something changes.
+     * saves our addition array to our cache and removes the full map from the cache
+     * leading to a regeneration of the map.
      *
-     * @param $resources
+     * @return void
      */
-    private function mergeResourcesWithAdditions($resources)
-    {
-        //var_dump($resources);
-        $this->addedResources['en'][] = 'core.en.odm';
-        foreach ($this->addedResources as $locale => $files) {
-            //var_dump($resources[$locale]); die;
-            foreach ($files as $file) {
-                //var_dump('/('.str_replace('.', '\\.', $file).')$/');
-                $hits =preg_grep('/$'.str_replace('.', '\\.', $file).'/', $resources[$locale]);
-                //var_dump($hits);
-            }
-        }
-
-        //die;
-    }
-
-    private function resourceIsInCache($resourcesHash)
-    {
-        $ret = false;
-
-        if ($this->cache->contains($this->cacheKeyFinalResourceHash) &&
-            $this->cache->fetch($this->cacheKeyFinalResourceHash) == $resourcesHash
-        ) {
-            $ret = true;
-        }
-
-        return $ret;
-    }
-
     private function persistAdditions()
     {
         $this->cache->save($this->cacheKey, $this->addedResources);
 
-        // invalidate full map hash -> forces regeneration of the whole thing
-        $this->cache->delete($this->cacheKeyFinalResourceHash);
+        // remove full map from cache
+        $this->cache->delete($this->cacheKeyFinalResource);
     }
 
+    /**
+     * processes all queued cache invalidations for the symfony translation cache.
+     * this is now only 1 Finder search for a single request.
+     *
+     * @return void
+     */
+    private function processInvalidations()
+    {
+        $fs = new Filesystem();
+        $localesToClean = array_keys($this->invalidations);
+        $deleteRegex = '/^catalogue\.(['.implode('|', $localesToClean).'])/';
+
+        try {
+            $finder = new Finder();
+            $finder
+                ->files()
+                ->in($this->cacheDirTranslations)
+                ->name($deleteRegex);
+
+            foreach ($finder as $file) {
+                $fs->remove($file->getRealPath());
+            }
+        } catch (\InvalidArgumentException $e) {
+            // happens when cache is non-existent
+        }
+    }
+
+    /**
+     * magic method that makes sure our file cleaning and persisting only
+     * happens near the end of the request, making it possible to only doing it once
+     * as opposed to everytime ;-)
+     * it's not necessary that it happens exactly at the end of the request as when this
+     * gets destructed all important things have already taken place.
+     *
+     * @return void
+     */
     public function __destruct()
     {
+        $this->processInvalidations();
+
         if ($this->isDirty === true) {
             $this->persistAdditions();
         }
