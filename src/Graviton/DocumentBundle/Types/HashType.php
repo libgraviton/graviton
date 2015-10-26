c98cfd5d3db78b1557a0247241d85632ebfb53f6
narcoticfresh <wee@xbe.ch>
2015-10-26 20:14:35 +0100

    recurse for extrefs in 'dynamic extrefs' in hashes.. pass extrefconverter to our type..

--- src/Graviton/DocumentBundle/Types/HashType.php
+++ src/Graviton/DocumentBundle/Types/HashType.php
@@ -6,7 +6,9 @@
 namespace Graviton\DocumentBundle\Types;
 
 use Doctrine\ODM\MongoDB\Types\Type;
+use Graviton\DocumentBundle\Entity\ExtReference;
 use Graviton\DocumentBundle\Entity\Hash;
+use Graviton\DocumentBundle\Service\ExtReferenceConverterInterface;
 
 /**
  * Hash type
@@ -17,6 +19,26 @@ use Graviton\DocumentBundle\Entity\Hash;
  */
 class HashType extends Type
 {
+
+    /**
+     * extref converter
+     *
+     * @var ExtReferenceConverterInterface
+     */
+    private static $extRefConverter;
+
+    /**
+     * sets the converter
+     *
+     * @param ExtReferenceConverterInterface $converter converter
+     *
+     * @return void
+     */
+    public function setExtRefConverter(ExtReferenceConverterInterface $converter)
+    {
+        self::$extRefConverter = $converter;
+    }
+
     /**
      * Convert DB value to PHP representation
      *
@@ -25,7 +47,7 @@ class HashType extends Type
      */
     public static function convertToPhp($value)
     {
-        return is_array($value) ? new Hash($value) : null;
+        return is_array($value) ? new Hash(self::processDynExtRefs($value)) : null;
     }
 
     /**
@@ -36,15 +58,65 @@ class HashType extends Type
      */
     public static function convertToDb($value)
     {
+        $dbValue = null;
+
         if (is_array($value)) {
-            return (object) $value;
+            $dbValue = (object) $value;
         } elseif ($value instanceof \ArrayObject) {
-            return (object) $value->getArrayCopy();
+            $dbValue = (object) $value->getArrayCopy();
         } elseif (is_object($value)) {
-            return (object) get_object_vars($value);
-        } else {
-            return null;
+            $dbValue = (object) get_object_vars($value);
         }
+
+        if (!is_null($dbValue)) {
+            $dbValue = (object) self::processDynExtRefs($dbValue);
+        }
+
+        return $dbValue;
+    }
+
+    /**
+     * loops our structure recursively to find all $ref objects that need to be converted
+     * either from that or to that..
+     *
+     * @param mixed $input input structure
+     *
+     * @return array altered structure with replaced $ref objects
+     */
+    public static function processDynExtRefs($input)
+    {
+        if ($input instanceof \stdClass) {
+            if (!empty(get_object_vars($input))) {
+                $input = self::processDynExtRefs(get_object_vars($input));
+            }
+            return $input;
+        }
+
+        $externalRefFieldName = '$ref';
+        $internalRefFieldName = '_dynExtRef';
+
+        if (is_array($input)) {
+            foreach ($input as $key => $value) {
+                if ($key === $internalRefFieldName) {
+                    if (is_array($value) && isset($value['$ref']) && isset($value['$id'])) {
+                        $extRef = ExtReference::create($value['$ref'], $value['$id']);
+                        $input[$externalRefFieldName] = self::$extRefConverter->getUrl($extRef);
+                        unset($input[$internalRefFieldName]);
+                    }
+                } elseif ($key === $externalRefFieldName) {
+                    $extRef = self::$extRefConverter->getExtReference($value);
+                    $input[$internalRefFieldName] = $extRef->jsonSerialize();
+                    unset($input[$externalRefFieldName]);
+                } else {
+                    if (is_array($value)) {
+                        $value = self::processDynExtRefs($value);
+                    }
+                    $input[$key] = $value;
+                }
+            }
+        }
+
+        return $input;
     }
 
     /**
