5cae60a5ba526fb9eeff3c7dc737da5c9e3edf3a
Samuel Ackermann <samuel.ackermann@swisscom.com>
2015-11-02 09:24:25 +0100

    added new validator for check optional booleans

---
+++ src/Graviton/RestBundle/Validator/Constraints/OptionalBooleanValidator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Validator for a strict boolean or null check (null because the value does not have to be set). The field is optional.
+ */
+
+namespace Graviton\RestBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validator for a strict boolean or null check.
+ *
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class OptionalBooleanValidator extends ConstraintValidator
+{
+    /**
+     * Check strictly for boolean or null. (null because the value does not have to be set)
+     *
+     * @param string     $value      Input value
+     * @param Constraint $constraint Constraint instance
+     *
+     * @return void
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if ($value !== true && $value !== false && $value !== null) {
+            $this->context->addViolation(
+                $constraint->message,
+                array('%string%' => $value)
+            );
+        }
+    }
+}
