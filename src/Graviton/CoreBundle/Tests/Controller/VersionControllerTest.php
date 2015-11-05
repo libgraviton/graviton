d75cfdfc48920b610f21d1e192d13b054848b8a5
narcoticfresh <wee@xbe.ch>
2015-11-05 11:38:52 +0100

    revert changes introduced in #325

--- src/Graviton/CoreBundle/Tests/Controller/VersionControllerTest.php
+++ src/Graviton/CoreBundle/Tests/Controller/VersionControllerTest.php
@@ -25,7 +25,7 @@ class VersionControllerTest extends RestTestCase
     public function testVersionsAction()
     {
         $client = static::createRestClient();
-        $client->request('GET', '/core/version/');
+        $client->request('GET', '/core/version');
         $response = $client->getResponse();
 
         $this->assertContains('"self":', $response->getContent());
