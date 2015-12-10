d8a55e4f9e6778d0d2c708d4e23e65e514241fac
Alexandr Evtushenko <aevtushenko@xiag.ch>
2015-12-10 12:46:14 +0600

    Add datetime serialization/deserialization test.

---
+++ src/Graviton/CoreBundle/Tests/Controller/DatetimeDeserializationControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * DatetimeDeserializationControllerTest class file
+ */
+
+namespace Graviton\CoreBundle\Tests\Controller;
+
+use Graviton\TestBundle\Test\RestTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use GravitonDyn\TestCaseDatetimeDeserializationBundle\Document\TestCaseDatetimeDeserialization;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class DatetimeDeserializationControllerTest extends RestTestCase
+{
+    /**
+     * Set up the test
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        if (!class_exists(TestCaseDatetimeDeserialization::class)) {
+            $this->markTestSkipped(sprintf('%s definition is not loaded', TestCaseDatetimeDeserialization::class));
+        }
+    }
+
+    /**
+     * Test datetime deserialization and serialization
+     *
+     * @return void
+     */
+    public function testDateDesrialization()
+    {
+        $data = (object) [
+            'datetime'  => '2015-12-10T12:02:16+0000',
+            'datetimes' => [
+                '2015-12-10T10:02:16+0000',
+                '2015-12-11T11:02:16+0000',
+                '2015-12-12T12:02:16+0000',
+            ],
+        ];
+
+        $client = static::createRestClient();
+        $client->post('/testcase/datetime-deserialization/', $data);
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertEquals($data, $client->getResults());
+    }
+}
