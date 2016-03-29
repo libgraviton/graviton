980f0e30796cfdf1d754adcd50c8ac12751c4dbd
Jacob Christensen <jacob.christensen@swisscom.com>
2016-03-29 16:49:34 +0200

    EVO-4410 Added to RabbitMQ coreId with users authenticated id

--- src/Graviton/RabbitMqBundle/Listener/EventStatusLinkResponseListener.php
+++ src/Graviton/RabbitMqBundle/Listener/EventStatusLinkResponseListener.php
@@ -11,12 +11,15 @@ use Graviton\DocumentBundle\Service\ExtReferenceConverter;
 use Graviton\RabbitMqBundle\Document\QueueEvent;
 use Graviton\RestBundle\HttpFoundation\LinkHeader;
 use Graviton\RestBundle\HttpFoundation\LinkHeaderItem;
+use Graviton\SecurityBundle\Authentication\Strategies\CookieFieldStrategy;
+use Graviton\SecurityBundle\Entities\SecurityUser;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use OldSound\RabbitMqBundle\RabbitMq\ProducerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
@@ -194,6 +197,7 @@ class EventStatusLinkResponseListener
         $obj->setEvent($this->generateRoutingKey());
         $obj->setDocumenturl($this->request->get('selfLink'));
         $obj->setStatusurl($this->getStatusUrl($obj));
+        $obj->setCoreId($this->getCoreId());
 
         return $obj;
     }
@@ -315,5 +319,23 @@ class EventStatusLinkResponseListener
             ->toArray();
 
         return array_keys($data);
+    }
+
+    /**
+     * Find current request attribute to User Core Id
+     *
+     * @return string
+     */
+    public function getCoreId()
+    {
+        $attributes = $this->request->attributes;
+        if (!$attributes) {
+            return '';
+        }
+        $value = $attributes->get(CookieFieldStrategy::CONFIGURATION_PARAMETER_ID);
+        if ($value) {
+            return (string) $value;
+        }
+        return '';
     }
 }
