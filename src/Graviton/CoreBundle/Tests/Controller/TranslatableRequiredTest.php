96ec03f087f56785361d87528b98a006e05f95da
narcoticfresh <wee@xbe.ch>
2016-08-29 11:30:18 +0200

    adapt tests, some validation messages have changed..

--- src/Graviton/CoreBundle/Tests/Controller/TranslatableRequiredTest.php
+++ src/Graviton/CoreBundle/Tests/Controller/TranslatableRequiredTest.php
@@ -112,7 +112,7 @@ class TranslatableRequiredTest extends RestTestCase
                     ]
                 ],
                 'complainField' => 'optional',
-                'errorMessage' => 'Array value found, but a null is required'
+                'errorMessage' => 'Array value found, but an object or a null is required'
             ],
             'empty-no-default' => [
                 'data' => [
