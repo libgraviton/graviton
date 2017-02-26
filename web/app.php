d23dd9fbe506343d1f622c420beb834e01b90c33
narcoticfresh <dn@xbe.ch>
2017-02-26 11:59:58 +0100

    first wave of updates..

--- web/app.php
+++ web/app.php
@@ -14,12 +14,11 @@ use Graviton\BundleBundle\Loader\BundleLoader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
 // @codingStandardsIgnoreStart
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+include_once __DIR__.'/../app/bootstrap.php.cache';
 // @codingStandardsIgnoreEnd
-
-require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
 
 // check for env
 $env = getenv('SYMFONY_ENV');
