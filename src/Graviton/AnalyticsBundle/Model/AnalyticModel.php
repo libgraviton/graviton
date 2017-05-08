d50ac6b715d07bb633c8d0e3263cc82808fff2cd
Jacob Christensen <Jacob.Christensen@swisscom.com>
2017-05-08 17:08:14 +0200

    Added optional Pipeline or Aggregation query.

--- src/Graviton/AnalyticsBundle/Model/AnalyticModel.php
+++ src/Graviton/AnalyticsBundle/Model/AnalyticModel.php
@@ -4,6 +4,8 @@
  */
 namespace Graviton\AnalyticsBundle\Model;
 
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+
 /**
  * Schema
  *
@@ -16,6 +18,7 @@ class AnalyticModel
     protected $collection;
     protected $route;
     protected $aggregate;
+    protected $pipeline;
     protected $schema;
     protected $type;
     protected $cacheTime;
@@ -78,6 +81,15 @@ class AnalyticModel
     }
 
     /**
+     * @param mixed $pipeline Data array for query
+     * @return void
+     */
+    public function setPipeline($pipeline)
+    {
+        $this->pipeline = $pipeline;
+    }
+
+    /**
      * Schema for response
      * @return mixed
      */
@@ -132,5 +144,62 @@ class AnalyticModel
     public function setCacheTime($cacheTime)
     {
         $this->cacheTime = (int) $cacheTime;
+    }
+
+
+
+    /**
+     * Build a output Db Model aggregation pipeline array.
+     *
+     * @return array
+     */
+    public function getPipeline()
+    {
+        $rtnPipeline = [];
+
+        if ($pipeline = $this->pipeline) {
+            foreach ($pipeline as $pipe) {
+                foreach ($pipe as $op => $query) {
+                    $rtnPipeline[] = [
+                        $op => $this->parseObjectDates($query)
+                    ];
+                }
+            }
+        } elseif ($aggregate = $this->getAggregate()) {
+            foreach ($aggregate as $op => $query) {
+                $rtnPipeline[] = [
+                    $op => $this->parseObjectDates($query)
+                ];
+            }
+        }
+
+        if (empty($rtnPipeline)) {
+            throw new  InvalidArgumentException('Wrong configuration for Aggregation pipeline');
+        }
+
+        return $rtnPipeline;
+    }
+
+    /**
+     * Enabling to possibility to create dtae queries
+     * Will replace PARSE_DATE(date|format)
+     * sample: PARSE_DATE(-4 years|Y) -> new DateTime(-4 years)->format(Y) -> 2013
+     *
+     * @param object $query Aggregation query
+     * @return object
+     */
+    private function parseObjectDates($query)
+    {
+        $string = json_encode($query);
+        preg_match_all('/PARSE_DATE\(([^\)]+)\)/', $string, $matches);
+        if ($matches && array_key_exists(1, $matches) && is_array($matches[1])) {
+            foreach ($matches[0] as $key => $value) {
+                $formatting = explode('|', $matches[1][$key]);
+                $date = new \DateTime($formatting[0]);
+                $string = str_replace('"'.$value.'"', $date->format($formatting[1]), $string);
+            }
+            $query = json_decode($string);
+        }
+        return $query;
     }
 }
