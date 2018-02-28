795c4983ad47972e1a857926b6fa6ec981caf2a4
narcoticfresh <dn@xbe.ch>
2018-02-28 14:20:49 +0100

    add new listener that handles the locking

--- src/Graviton/RestBundle/Listener/WriteLockListener.php
+++ src/Graviton/RestBundle/Listener/WriteLockListener.php
@@ -1,27 +1,16 @@
 <?php
 /**
- * listener that implements write locks on data altering requests with PUT and PATCH methods.
+ * listener that implements write locks on data altering requests with PUT and PATCH methods
+ * and adds random waits to certain operations.
  */
-
 namespace Graviton\RestBundle\Listener;
 
+use Doctrine\Common\Cache\CacheProvider;
 use Monolog\Logger;
-use Psr\Http\Message\MessageInterface;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\Lock\Factory;
-use Symfony\Component\Lock\Store\SemaphoreStore;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Graviton\RestBundle\HttpFoundation\LinkHeader;
-use Graviton\RestBundle\HttpFoundation\LinkHeaderItem;
-use Graviton\RestBundle\Event\RestEvent;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
@@ -41,49 +30,168 @@ class WriteLockListener
      */
     private $requestStack;
 
+    /**
+     * @var CacheProvider
+     */
+    private $cache;
+
+    /**
+     * on these methods, we will create a lock and wait
+     *
+     * @var array
+     */
     private $lockingMethods = [
         Request::METHOD_PUT,
         Request::METHOD_PATCH
     ];
 
     /**
-     * @param Router $router router
+     * on these methods, we will just wait..
+     *
+     * @var array
      */
-    public function __construct(Logger $logger, RequestStack $requestStack)
-    {
+    private $waitingMethods = [
+        Request::METHOD_GET,
+        Request::METHOD_DELETE
+    ];
+
+    /**
+     * all methods we are interested in
+     *
+     * @var array
+     */
+    private $interestedMethods = [];
+
+    /**
+     * on these urls, we make a randomwait to randomly delay multiple incoming requests
+     *
+     * @var array
+     */
+    private $randomWaitUrls = [];
+
+    /**
+     * @var string
+     */
+    private $cacheKeyPrefix = 'writeLock-';
+
+    /**
+     * @var int
+     */
+    private $maxTime = 10;
+
+    /**
+     * @var int minimal delay in milliseconds
+     */
+    private $randomDelayMin = 200;
+
+    /**
+     * @var int maximal delay in milliseconds
+     */
+    private $randomDelayMax = 500;
+
+    /**
+     * @param Logger        $logger         logger
+     * @param RequestStack  $requestStack   request stack
+     * @param CacheProvider $cache          cache
+     * @param array         $randomWaitUrls urls we randomly wait on
+     */
+    public function __construct(
+        Logger $logger,
+        RequestStack $requestStack,
+        CacheProvider $cache,
+        array $randomWaitUrls
+    ) {
         $this->logger = $logger;
         $this->requestStack = $requestStack;
-
-        $store = new SemaphoreStore();
-        $this->factory = new Factory($store);
+        $this->cache = $cache;
+        $this->interestedMethods = array_merge($this->lockingMethods, $this->waitingMethods);
+        $this->randomWaitUrls = $randomWaitUrls;
     }
 
     /**
-     * add a rel=self Link header to the response
+     * all "waiting" methods wait until no lock is around.. "writelock" methods wait and create a lock
      *
-     * @param FilterResponseEvent      $event      response listener event
-     * @param string                   $eventName  event name
-     * @param EventDispatcherInterface $dispatcher dispatcher
+     * @param FilterControllerEvent $event response listener event
      *
      * @return void
      */
     public function onKernelController(FilterControllerEvent $event)
     {
-        if (!in_array($this->requestStack->getCurrentRequest()->getMethod(), $this->lockingMethods)) {
+        $currentMethod = $this->requestStack->getCurrentRequest()->getMethod();
+
+        // ignore not defined methods..
+        if (!in_array($currentMethod, $this->interestedMethods)) {
             return;
         }
 
         $url = $this->requestStack->getCurrentRequest()->getPathInfo();
-        $lock = $this->factory->createLock($url);
-        var_dump($event->getController());
+        $cacheKey = $this->cacheKeyPrefix.$url;
 
+        // should we do a random delay here? only applies to writing methods!
+        if (in_array($currentMethod, $this->lockingMethods) &&
+            $this->doRandomDelay($url)
+        ) {
+            $delay = rand($this->randomDelayMin, $this->randomDelayMax) * 1000;
+            $this->logger->info("LOCK CHECK DELAY BY ".$delay." = ".$cacheKey);
 
-        echo "dude"; die;
+            usleep(rand($this->randomDelayMin, $this->randomDelayMax) * 1000);
+        }
+
+        $this->logger->info("LOCK CHECK START = ".$cacheKey);
+
+        // check for existing one
+        while ($this->cache->fetch($cacheKey) === true) {
+            usleep(250000);
+        }
+
+        $this->logger->info("LOCK CHECK FINISHED = ".$cacheKey);
+
+        if (in_array($currentMethod, $this->waitingMethods)) {
+            // current method just wants to wait..
+            return;
+        }
+
+        // create new
+        $this->cache->save($cacheKey, true, $this->maxTime);
+        $this->logger->info("LOCK ADD = ".$cacheKey);
+
+        $event->getRequest()->attributes->set('writeLockOn', $cacheKey);
     }
 
+    /**
+     * release the lock
+     *
+     * @param FilterResponseEvent $event response listener event
+     *
+     * @return void
+     */
     public function onKernelResponse(FilterResponseEvent $event)
     {
-        $url = $this->requestStack->getCurrentRequest()->getPathInfo();
+        $lockName = $event->getRequest()->attributes->get('writeLockOn', null);
+        if (!is_null($lockName)) {
+            $this->cache->delete($lockName);
+            $this->logger->info("LOCK REMOVED = ".$lockName);
+        }
     }
 
+    /**
+     * if we should randomly wait on current request
+     *
+     * @param string $url the current url
+     *
+     * @return boolean true if yes, false otherwise
+     */
+    private function doRandomDelay($url)
+    {
+        return array_reduce(
+            $this->randomWaitUrls,
+            function ($carry, $value) use ($url) {
+                if ($carry !== true) {
+                    return (strpos($url, $value) === 0);
+                } else {
+                    return $carry;
+                }
+            }
+        );
+    }
 }
