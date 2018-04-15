90c0600e11a48bc4021bd52d232c92ff6fcc121e
narcoticfresh <dn@xbe.ch>
2018-04-15 10:56:59 +0200

    execute doctrine command

--- dev-cleanstart.sh
+++ dev-cleanstart.sh
@@ -1,5 +1,5 @@
 rm -Rf app/cache app/logs app/bootstrap.php.cache
 composer install
 php app/console doctrine:mongodb:fixtures:load
-php app/console graviton:generate:build-indexes
+php app/console doctrine:mongodb:schema:update
 php app/console server:run
