42b576ec8d7ec5580cdd89707e72c3967889583d
narcoticfresh <dn@xbe.ch>
2018-06-04 08:14:05 +0200

    dump results

--- src/Graviton/AnalyticsBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Graviton/AnalyticsBundle/Tests/Controller/DefaultControllerTest.php
@@ -458,6 +458,8 @@ class DefaultControllerTest extends RestTestCase
 
         $results = $client->getResults();
 
+        var_dump($results);
+
         // make sure it has been resolved
         $this->assertTrue(isset($results[0]->contact->type));
         $this->assertTrue(isset($results[0]->contacts[0]->type));
