2d6f8f0522372fbcc9526ede50176be55cf22263
narcoticfresh <dn@xbe.ch>
2018-09-05 10:31:03 +0200

    try to catch other alphanumerics

--- src/Graviton/DocumentBundle/Service/SolrQuery.php
+++ src/Graviton/DocumentBundle/Service/SolrQuery.php
@@ -196,9 +196,10 @@ class SolrQuery
             return $term;
         }
 
-        // everything that is only numbers *and* uppercase characters and at least 3 long, we don't fuzzy/wildcard
-        $pattern = '/^[A-Z0-9]{3,}$/';
-        if (preg_match($pattern, $term, $matches) === 1) {
+        // everything that is only numbers *and* characters and at least 3 long, we don't fuzzy/wildcard
+        // thanks to https://stackoverflow.com/a/7684859/3762521
+        $pattern = '/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/';
+        if (strlen($term) > 3 && preg_match($pattern, $term, $matches) === 1) {
             return $term;
         }
 
