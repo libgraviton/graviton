e1507316bce12b103a929af6954054022a2a6994
narcoticfresh <dn@xbe.ch>
2018-09-28 13:42:42 +0200

    rename and change translatable fixture loaders

---
+++ src/Graviton/I18nBundle/DataFixtures/MongoDB/LoadTranslationLanguageData.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * /core/app fixtures for mongodb app collection.
+ */
+
+namespace Graviton\I18nBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Graviton\I18nBundle\Document\Translation;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class LoadTranslationLanguageData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @param ObjectManager $manager Object Manager
+     *
+     * @return void
+     */
+    public function load(ObjectManager $manager)
+    {
+        $data = [
+            [
+                'language' => 'de',
+                'original' => 'German',
+                'localized' => 'Deutsch'
+            ],
+            [
+                'language' => 'fr',
+                'original' => 'English',
+                'localized' => 'Anglais'
+            ],
+            [
+                'language' => 'es',
+                'original' => 'English',
+                'localized' => 'Inglés'
+            ]
+        ];
+
+        foreach ($data as $record) {
+            $translation = new Translation();
+            $translation->setLanguage($record['language']);
+            $translation->setOriginal($record['original']);
+            $translation->setLocalized($record['localized']);
+            $manager->persist($translation);
+            $manager->flush();
+        }
+    }
+}
