099444700a4e34e3e87ab52426fe79d03accd41e
narcoticfresh <dn@xbe.ch>
2018-09-28 14:32:52 +0200

    add Translation document

---
+++ src/Graviton/I18nBundle/Model/Translation.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Model based on Graviton\RestBundle\Model\DocumentModel.
+ */
+
+namespace Graviton\I18nBundle\Model;
+
+use Graviton\RestBundle\Model\DocumentModel;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class Translation extends DocumentModel
+{
+}
