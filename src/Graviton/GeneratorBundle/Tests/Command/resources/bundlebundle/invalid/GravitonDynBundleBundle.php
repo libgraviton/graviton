46aa50ad3347a7abeadb9069089722e3e0985557
narcoticfresh <dn@xbe.ch>
2018-11-07 12:48:52 +0100

    add test resources

---
+++ src/Graviton/GeneratorBundle/Tests/Command/resources/bundlebundle/invalid/GravitonDynBundleBundle.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * bundle class
+ */
+
+namespace somenamespace;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Graviton\BundleBundle\GravitonBundleInterface;
+
+/**
+ * Graviton dynamic BundleBundle - DO NOT MANIPULATE!
+ *
+ * @category GravitonDynBundleBundle
+ * @author   tester <test@test.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class GravitonDynBundleBundle extends Bundle implements GravitonBundleInterface
+{
+    /**
+     * set up graviton symfony dynamic bundles
+     *
+     * @return \Symfony\Component\HttpKernel\Bundle\Bundle[]
+     */
+    public function getBundles()
+    {
+        return array(
+            new SomeBundle()
+        );
+    }
+}
