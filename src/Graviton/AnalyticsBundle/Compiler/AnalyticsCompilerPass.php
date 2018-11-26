2b332e34eaa0da6f99e5c373e7ca6dc4933082ac
narcoticfresh <dn@xbe.ch>
2018-11-26 09:56:20 +0100

    add compilerpass for analytics services

---
+++ src/Graviton/AnalyticsBundle/Compiler/AnalyticsCompilerPass.php
@@ -0,0 +1,46 @@
+<?php
+/** compiler pass for analytics services */
+
+namespace Graviton\AnalyticsBundle\Compiler;
+
+use Graviton\AnalyticsBundle\Model\AnalyticModel;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class AnalyticsCompilerPass implements CompilerPassInterface
+{
+
+    public function process(ContainerBuilder $container)
+    {
+		$services = [];
+		$scanDir = __DIR__.'/../../../../../../';
+
+		$finder = Finder::create()
+			->files()
+			->in($scanDir)
+			->path('/\/analytics\//i')
+			->name('*.json')
+			->notName('_*')
+			->sortByName();
+
+		foreach ($finder as $file) {
+			$key = $file->getFilename();
+			$data = json_decode($file->getContents(), true);
+			if (json_last_error()) {
+				throw new InvalidConfigurationException(
+					sprintf('Analytics file: %s could not be loaded due to error: %s', $key, json_last_error_msg())
+				);
+			}
+			$services[$data['route']] = $data;
+		}
+
+		$container->setParameter('analytics.services', $services);
+    }
+}
--- src/Graviton/AnalyticsBundle/GravitonAnalyticsBundle.php
+++ src/Graviton/AnalyticsBundle/GravitonAnalyticsBundle.php
@@ -4,6 +4,8 @@
  */
 namespace Graviton\AnalyticsBundle;
 
+use Graviton\AnalyticsBundle\Compiler\AnalyticsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -15,4 +17,16 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class GravitonAnalyticsBundle extends Bundle
 {
+	/**
+	 * load version compiler pass
+	 *
+	 * @param ContainerBuilder $container container builder
+	 *
+	 * @return void
+	 */
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+		$container->addCompilerPass(new AnalyticsCompilerPass());
+	}
 }
