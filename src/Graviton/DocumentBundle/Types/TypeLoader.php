fba216ae9b4d6be5718cd56c50c882d011068bed
Dario Nuevo <wee@xbe.ch>
2019-05-06 16:08:49 +0200

    move things into a typeloader

---
+++ src/Graviton/DocumentBundle/Types/TypeLoader.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * doctrine type loader
+ */
+
+namespace Graviton\DocumentBundle\Types;
+
+use Doctrine\ODM\MongoDB\Types\Type;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class TypeLoader
+{
+
+    public static function load()
+    {
+        Type::registerType('extref', ExtReferenceType::class);
+        Type::registerType('translatable', TranslatableType::class);
+        Type::registerType('translatablearray', TranslatableArrayType::class);
+        Type::registerType('hash', HashType::class);
+        Type::registerType('hasharray', HashArrayType::class);
+        Type::registerType('datearray', DateArrayType::class);
+    }
+
+}
