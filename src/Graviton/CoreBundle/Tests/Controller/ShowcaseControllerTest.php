ac7014bbe43ec432973d205ba56ce8b0e882c5f3
Dario Nuevo <dn@xbe.ch>
2019-11-15 12:27:18 +0100

    don't expect empty arrays anymore

--- src/Graviton/CoreBundle/Tests/Controller/ShowcaseControllerTest.php
+++ src/Graviton/CoreBundle/Tests/Controller/ShowcaseControllerTest.php
@@ -481,17 +481,6 @@ class ShowcaseControllerTest extends RestTestCase
         $client = static::createRestClient();
         $client->request('GET', '/hans/showcase/?'.$rqlSelect);
 
-        /* expect empty arrays */
-        $filtred = array_map(
-            function ($entry) {
-                $entry->contacts = [];
-                $entry->nestedArray = [];
-                $entry->nestedApps = [];
-                return $entry;
-            },
-            $filtred
-        );
-
         $this->assertEquals($filtred, $client->getResults());
 
         foreach ([
