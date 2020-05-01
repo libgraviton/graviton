14646648d552d15946756fc3e66122c7a9d17ad7
Dario Nuevo <dn@xbe.ch>
2020-05-01 16:41:38 +0200

    extend our abstract with facilities to get the context and thus access necessary stuff for the actual listeners

---
+++ src/Graviton/RestBundle/Listener/DynService/RestListenerAbstract.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * abstract class for dynamic service rest listeners
+ */
+
+namespace Graviton\RestBundle\Listener\DynService;
+
+use Graviton\RestBundle\Event\EntityPrePersistEvent;
+use Graviton\RestBundle\Listener\DynServiceRestListener;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+abstract class RestListenerAbstract
+{
+
+    /**
+     * @var DynServiceRestListener
+     */
+    private $context;
+
+    /**
+     * get Context
+     *
+     * @return DynServiceRestListener Context
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     * set Context
+     *
+     * @param DynServiceRestListener $context context
+     *
+     * @return void
+     */
+    public function setContext($context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * called before the entity is persisted
+     *
+     * @param EntityPrePersistEvent $event event
+     *
+     * @return EntityPrePersistEvent event
+     */
+    public function prePersist(EntityPrePersistEvent $event)
+    {
+        return $event;
+    }
+}
