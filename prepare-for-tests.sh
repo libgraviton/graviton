a2a3b515453d8d1b9a2953fdcee711ac111c574a
Dario Nuevo <dn@xbe.ch>
2020-05-06 10:58:12 +0200

    aaand.. try again without duplications

--- prepare-for-tests.sh
+++ prepare-for-tests.sh
@@ -1,6 +1,2 @@
 rm -Rf src/GravitonDyn
-export GENERATOR_SYNTHETIC_FIELDS='int:clientId'
-export GENERATOR_SYNTHETIC_FIELDS_EXPOSE_ON="/testcase/rest-listeners-cond-persister"
-export DATA_RESTRICTION_MAP="{x-graviton-client: 'int:clientId'}"
-composer configure
-composer install --ignore-platform-reqs --no-interaction
+SYMFONY_ENV=test composer install --ignore-platform-reqs --no-interaction
