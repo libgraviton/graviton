f7090cfa2b80a5706fb9b4ac3fe90581f57e4788
Dario Nuevo <wee@xbe.ch>
2022-10-17 14:50:28 +0200

    use param directly

--- src/Graviton/DocumentBundle/DependencyInjection/GravitonDocumentExtension.php
+++ src/Graviton/DocumentBundle/DependencyInjection/GravitonDocumentExtension.php
@@ -6,7 +6,6 @@
 namespace Graviton\DocumentBundle\DependencyInjection;
 
 use Graviton\BundleBundle\DependencyInjection\GravitonBundleExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -27,38 +26,5 @@ class GravitonDocumentExtension extends GravitonBundleExtension
     public function getConfigDir()
     {
         return __DIR__ . '/../Resources/config';
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * Overwrite mongodb config from parent in cloud case.
-     *
-     * @param ContainerBuilder $container instance
-     *
-     * @return void
-     */
-    public function prepend(ContainerBuilder $container)
-    {
-        parent::prepend($container);
-
-        // grab mongo config directly from vcap...
-        $services = getenv('VCAP_SERVICES');
-        if (!empty($services)) {
-            $services = json_decode($services, true);
-            $mongo = $services['mongodb'][0]['credentials'];
-
-            $container->setParameter('mongodb.default.server.uri', $mongo['uri']);
-            $container->setParameter('mongodb.default.server.db', $mongo['database']);
-        } else {
-            $container->setParameter(
-                'mongodb.default.server.uri',
-                $container->getParameter('graviton.mongodb.default.server.uri')
-            );
-            $container->setParameter(
-                'mongodb.default.server.db',
-                $container->getParameter('graviton.mongodb.default.server.db')
-            );
-        }
     }
 }
