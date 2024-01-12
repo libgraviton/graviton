d77da8e6135e925c177b3411e006be06e0fa232c
Dario Nuevo <wee@xbe.ch>
2024-01-12 16:38:55 +0100

    no more constructor

--- src/Graviton/CoreBundle/Model/Dummy.php
+++ src/Graviton/CoreBundle/Model/Dummy.php
@@ -14,11 +14,4 @@ use Graviton\RestBundle\Model\DocumentModel;
  */
 class Dummy extends DocumentModel
 {
-    /**
-     * constructor
-     */
-    public function __construct()
-    {
-        parent::__construct(__DIR__.'/../Resources/config/schema/Dummy.json');
-    }
 }
